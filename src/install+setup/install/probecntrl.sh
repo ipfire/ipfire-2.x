76ed51e8f6b2f7e072f4c4da92d9abfde6e5c590
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-03-11 20:09:05 +0000

    OpenSwan RC nach den Vorgaben von "affect" installiert. Zurueck zu ReiserFS, da wir mit Reiser4 noch ein paar Probleme haben und wir die Prioritaeten noch auf andere Sachen legen muessen. :'(
    git-svn-id: http://svn.ipfire.org/svn/ipfire/trunk@445 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- src/install+setup/install/probecntrl.sh
+++ src/install+setup/install/probecntrl.sh
@@ -4,7 +4,7 @@ echo "Probing for SCSI controllers"
 MODULE=`/bin/kudzu -qps  -t 30 -c SCSI | grep driver | cut -d ' ' -f 2 | sort | uniq`
 
 if [ "$MODULE" ]; then
-	echo $MODULE > /cntrldriver
+	echo $MODULE > /tmp/cntrldriver
 	echo "Your controller is: $MODULE"
 	exit 0
 fi
--- src/install+setup/install/probenic.sh
+++ src/install+setup/install/probenic.sh
@@ -3,16 +3,16 @@
 MODULES=$(/bin/kudzu -qps  -t 30 -c NETWORK | grep driver | cut -d ' ' -f 2 | sort)
 
 if [ "$1" == "count" ]; then
-	/bin/kudzu -qps  -t 30 -c NETWORK | grep driver | wc -l | awk '{ print $1 }' > /drivercount
+	/bin/kudzu -qps  -t 30 -c NETWORK | grep driver | wc -l | awk '{ print $1 }' > /tmp/drivercount
 	exit 0
 else
 	NUMBER=$1
 fi
 
 if [ "$NUMBER" ]; then
-	echo "$(echo $MODULES | grep -n $NUMBER | cut -c 1-2 )" > /nicdriver
+	echo "$(echo $MODULES | grep -n $NUMBER | cut -c 1-2 )" > /tmp/nicdriver
 else
-	echo "$MODULES" > /nicdriver
+	echo "$MODULES" > /tmp/nicdriver
 fi
 
 #  kudzu -qps -c NETWORK | egrep "desc|network.hwaddr|driver" | awk -F': ' '{print $2}' | sed -e '/..:..:..:..:..:../a\\' -e "s/$/\;/g" | tr "\n" "XX" | sed -e "s/XX/\n/g" -e "s/\;X/\;/g"
