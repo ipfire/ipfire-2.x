f7ff12a35788904140b2879f93cbd47eab9cd61d
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-05-01 22:04:01 +0000

    Clamav-Fixes. Clamavctrl gebaut & Initscript geschrieben.
    git-svn-id: http://svn.ipfire.org/svn/ipfire/trunk@513 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

---
+++ src/misc-progs/clamavctrl.c
@@ -0,0 +1,44 @@
+/* This file is part of the IPFire Firewall.
+ *
+ * This program is distributed under the terms of the GNU General Public
+ * Licence.  See the file COPYING for details.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include "setuid.h"
+
+int main(int argc, char *argv[]) {
+
+	if (!(initsetuid()))
+		exit(1);
+
+	if (argc < 2) {
+		fprintf(stderr, "\nNo argument given.\n\nclamavctrl (start|stop|restart)\n\n");
+		exit(1);
+	}
+
+	if (strcmp(argv[1], "start") == 0) {
+		safe_system("/etc/rc.d/init.d/clamav start");
+	} else if (strcmp(argv[1], "stop") == 0) {
+		safe_system("/etc/rc.d/init.d/clamav stop");
+	} else if (strcmp(argv[1], "restart") == 0) {
+		safe_system("/etc/rc.d/init.d/clamav restart");
+	} else if (strcmp(argv[1], "enable") == 0) {
+		safe_system("ln -fs ../init.d/clamav /etc/rc.d/rc3.d/S20clamav >/dev/null 2>&1");
+		safe_system("ln -fs ../init.d/clamav /etc/rc.d/rc0.d/K80clamav >/dev/null 2>&1");
+		safe_system("ln -fs ../init.d/clamav /etc/rc.d/rc6.d/K80clamav >/dev/null 2>&1");
+	} else if (strcmp(argv[1], "disable") == 0) {
+		safe_system("rm -f /etc/rc.d/rc*.d/*clamav >/dev/null 2>&1");
+	} else {
+		fprintf(stderr, "\nBad argument given.\n\nclamavctrl (start|stop|restart)\n\n");
+		exit(1);
+	}
+
+	return 0;
+}
