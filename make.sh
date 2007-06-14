0e772a80d88bc7d69ccce9e9f0bf767bfa93cc3b
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-06-14 09:41:09 +0000

    Noch ein paar Rand-Cryptop-Engines aktiviert. Testing4.
    git-svn-id: http://svn.ipfire.org/svn/ipfire/trunk@628 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- make.sh
+++ make.sh
@@ -24,7 +24,7 @@
 
 NAME="IPFire"				# Software name
 SNAME="ipfire"			# Short name
-VERSION="2.0a3"			# Version number
+VERSION="2.0t4"			# Version number
 SLOGAN="www.ipfire.org"		# Software slogan
 CONFIG_ROOT=/var/ipfire		# Configuration rootdir
 NICE=10				# Nice level
