4fe9acb28327d81c6fb130d415aa466118438a1d
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-09-01 10:48:01 +0000

    Die letzten "missing_rootfile"s berichtigt und angelegt. NFS-Paket geschnuert.
    git-svn-id: http://svn.ipfire.org/svn/ipfire/trunk@870 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- make.sh
+++ make.sh
@@ -583,7 +583,7 @@ buildpackages() {
 	fi
   done
   echo "== List of softwares used to build $NAME Version: $VERSION ==" > $BASEDIR/doc/packages-list.txt
-  grep -v 'configroot$\|img$\|initrd$\|initscripts$\|installer$\|install$\|setup$\|pakfire$\|stage2$\|smp$\|tools$\|tools1$\|tools2$\|.tgz$\|-config$\|_missing_rootfile$' \
+  grep -v 'configroot$\|img$\|initrd$\|initscripts$\|installer$\|install$\|setup$\|pakfire$\|stage2$\|smp$\|tools$\|tools1$\|tools2$\|.tgz$\|-config$\|_missing_rootfile$\|install1$\|install2$\|pass1$\|pass2$\|pass3$' \
 	$BASEDIR/doc/packages-list | sort >> $BASEDIR/doc/packages-list.txt
   rm -f $BASEDIR/doc/packages-list
   # packages-list.txt is ready to be displayed for wiki page
