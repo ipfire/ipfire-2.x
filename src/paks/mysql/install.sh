203c2787c97b5d026fe477f4765f2fc295d9917a
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-10-14 14:04:38 +0000

    Clamav kommt erstma ohne Virendefinitionen daher. Die laedt der sich dann alleine. rsync-Update. Das Pakfire-Update kann beim Verbinden auch im Hintergrund ablaufen. Einige Shell-Kommandos interaktiv gemacht. AdoDB ins Paket von Openmailadmin hinzugefuegt. Prompt ist wieder farbig. Das MySQL-Setup ist noch nicht so besonders fit. Der Server braucht zu lange zum starten - somit kann noch kein Passwort gesetzt werden.
    git-svn-id: http://svn.ipfire.org/svn/ipfire/trunk@971 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- src/paks/mysql/install.sh
+++ src/paks/mysql/install.sh
@@ -9,6 +9,16 @@ ln -svf  ../init.d/mysql /etc/rc.d/rc6.d/K26mysql
 
 /etc/init.d/mysql start
 
-sleep 10
+COUNTER=0
+
+while [ "$COUNTER" -lt "10" ]; do
+	[ -e "/var/run/mysql/mysql.sock" ] && break
+	echo "MySQL server is still not running. Waiting 5 seconds."
+	sleep 5
+	(( $COUNTER += 1 ))
+done 
+
+[ -e "/var/run/mysql/mysql.sock" ] || (echo "MySQL still noch running... Exiting."; \
+	exit 1)
 
 mysqladmin -u root --password='' password 'mysqlfire'
