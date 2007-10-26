7f5fdd04d23b54a88668338ad98e280495727420
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-10-26 22:26:23 +0000

    Added some custom patches for openswan tagged rc2 fixed red and cleanfs by arne
    git-svn-id: http://svn.ipfire.org/svn/ipfire/trunk@1002 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- make.sh
+++ make.sh
@@ -24,7 +24,7 @@
 
 NAME="IPFire"										# Software name
 SNAME="ipfire"									# Short name
-VERSION="2.0rc2t"								# Version number
+VERSION="2.0rc2"								# Version number
 SLOGAN="www.ipfire.org"					# Software slogan
 CONFIG_ROOT=/var/ipfire					# Configuration rootdir
 NICE=10													# Nice level
