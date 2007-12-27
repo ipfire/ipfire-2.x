071e69ccf63c35b52258b0644b4b005c3398e3ba
ms <ms@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2007-12-27 02:07:57 +0000

    Core Update 3: Fixed NFS and portmap.
    git-svn-id: http://svn.ipfire.org/svn/ipfire/branches/2.1/trunk@1130 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

---
+++ config/rootfiles/core/3/update.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+. /opt/pakfire/lib/functions.sh
+extract_files
---
+++ src/paks/portmap/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+############################################################################
+#                                                                          #
+# This file is part of the IPFire Firewall.                                #
+#                                                                          #
+# IPFire is free software; you can redistribute it and/or modify           #
+# it under the terms of the GNU General Public License as published by     #
+# the Free Software Foundation; either version 2 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# IPFire is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+# GNU General Public License for more details.                             #
+#                                                                          #
+# You should have received a copy of the GNU General Public License        #
+# along with IPFire; if not, write to the Free Software                    #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA #
+#                                                                          #
+# Copyright (C) 2007 IPFire-Team <info@ipfire.org>.                        #
+#                                                                          #
+############################################################################
+#
+. /opt/pakfire/lib/functions.sh
+extract_files
+start_service --background ${NAME}
+ln -sf  ../init.d/portmap /etc/rc.d/rc0.d/K49portmap
+ln -sf  ../init.d/portmap /etc/rc.d/rc3.d/S22portmap
+ln -sf  ../init.d/portmap /etc/rc.d/rc6.d/K49portmap
