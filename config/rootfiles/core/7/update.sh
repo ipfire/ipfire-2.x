fedc56d69cbcdfb8b44f3475682f8cb0d2a2d85f
arne_f <arne_f@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2008-02-02 22:20:25 +0000

    changed permissions of html/graphs to match with original final.iso
    git-svn-id: http://svn.ipfire.org/svn/ipfire/branches/2.1/trunk@1180 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- config/rootfiles/core/7/update.sh
+++ config/rootfiles/core/7/update.sh
@@ -4,5 +4,4 @@ extract_files
 GATEWAY=$(cat /var/ipfire/red/remote-ipaddress)
 echo "$GATEWAY gateway" >> /etc/hosts
 /etc/init.d/collectd start
-chown -R nobody:nobody /srv/web/ipfire/html/graphs
 perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
