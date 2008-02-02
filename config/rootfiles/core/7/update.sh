25538a4b2a81a3e19f7ccf9f89cb7960068deb8b
arne_f <arne_f@ea5c0bd1-69bd-2848-81d8-4f18e57aeed8>
2008-02-02 18:19:04 +0000

    Fixed collectd rc.d links
    git-svn-id: http://svn.ipfire.org/svn/ipfire/branches/2.1/trunk@1176 ea5c0bd1-69bd-2848-81d8-4f18e57aeed8

--- config/rootfiles/core/7/update.sh
+++ config/rootfiles/core/7/update.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 . /opt/pakfire/lib/functions.sh
 extract_files
-ln -s /etc/rc.d/rc0.d/K50collectd /etc/init.d/collectd
-ln -s /etc/rc.d/rc3.d/S20collectd /etc/init.d/collectd
-ln -s /etc/rc.d/rc6.d/K50collectd /etc/init.d/collectd
 GATEWAY=$(cat /var/ipfire/red/remote-ipaddress)
 echo "$GATEWAY gateway" >> /etc/hosts
 /etc/init.d/collectd start
