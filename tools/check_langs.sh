8fc2cdd6b0175cdb624fe64b37c55069b4f4be7c
Michael Tremer <michael.tremer@ipfire.org>
2008-04-09 23:29:30 +0200

    Removed deletion of the old .svn-subdirectories.

--- tools/check_langs.sh
+++ tools/check_langs.sh
@@ -29,7 +29,6 @@ cat ./langs/de/cgi-bin/de.pl | grep \'.*\' | awk -F\' '{print $2}'| sort > /tmp/
 cat ./langs/de/install/lang_de.c | grep TR_ | awk -F\  '{print $2}' > /tmp/de_install.$$
 
 for i in ./langs/en; do
-    if [ "$i" == "./langs/.svn" ] ; then continue; fi
     language=`echo "$i" | awk -F/  '{ print $3 }'`
 
     echo "############################################################################"
