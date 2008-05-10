2f780a679787858cc7f4f1016cace1283e1b9881
Arne Fitzenreiter <arne_f@ipfire.org>
2008-05-11 01:19:22 +0200

    Remove obsolete dvb-modul, fixed core15 files list

--- config/rootfiles/core/15/update.sh
+++ config/rootfiles/core/15/update.sh
@@ -2,8 +2,9 @@
 . /opt/pakfire/lib/functions.sh
 /usr/local/bin/backupctrl exclude >/dev/null 2>&1
 rm /lib/modules/2.6.16.57-ipfire/kernel/drivers/media/video/video-buf.ko
+rm /lib/modules/2.6.16.57-ipfire/kernel/drivers/media/dvb/frontends/dib3000-common.ko
 rm /lib/modules/2.6.16.57-ipfire-smp/kernel/drivers/media/video/video-buf.ko
+rm /lib/modules/2.6.16.57-ipfire-smp/kernel/drivers/media/dvb/frontends/dib3000-common.ko
 extract_files
 depmod -a 2.6.16.57-ipfire
 depmod -a 2.6.16.57-ipfire-smp
-
