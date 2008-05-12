66df69bc2d9eec1072ec0bf9e1d82e45b8e3aa75
Arne Fitzenreiter <arne_f@ipfire.org>
2008-05-12 12:33:17 +0200

    fixed v4l-dvb config file, exchanged core14 <-> core15

--- config/rootfiles/core/15/update.sh
+++ config/rootfiles/core/15/update.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 . /opt/pakfire/lib/functions.sh
 /usr/local/bin/backupctrl exclude >/dev/null 2>&1
-rm /lib/modules/2.6.16.57-ipfire/kernel/drivers/media/video/video-buf.ko
-rm /lib/modules/2.6.16.57-ipfire/kernel/drivers/media/dvb/frontends/dib3000-common.ko
-rm /lib/modules/2.6.16.57-ipfire-smp/kernel/drivers/media/video/video-buf.ko
-rm /lib/modules/2.6.16.57-ipfire-smp/kernel/drivers/media/dvb/frontends/dib3000-common.ko
 extract_files
-depmod -a 2.6.16.57-ipfire
-depmod -a 2.6.16.57-ipfire-smp
+/etc/init.d/mISDN config
