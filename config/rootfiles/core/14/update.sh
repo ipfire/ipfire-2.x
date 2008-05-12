76f912a0c10c5d5423fce111a6338d151a416e15
Arne Fitzenreiter <arne_f@ipfire.org>
2008-05-12 12:44:02 +0200

    Moved squid update from core15 back to core14

--- config/rootfiles/core/14/update.sh
+++ config/rootfiles/core/14/update.sh
@@ -5,6 +5,8 @@ rm /lib/modules/2.6.16.57-ipfire/kernel/drivers/media/video/video-buf.ko
 rm /lib/modules/2.6.16.57-ipfire/kernel/drivers/media/dvb/frontends/dib3000-common.ko
 rm /lib/modules/2.6.16.57-ipfire-smp/kernel/drivers/media/video/video-buf.ko
 rm /lib/modules/2.6.16.57-ipfire-smp/kernel/drivers/media/dvb/frontends/dib3000-common.ko
+/etc/init.d/squid stop
 extract_files
+/etc/init.d/squid start
 depmod -a 2.6.16.57-ipfire
 depmod -a 2.6.16.57-ipfire-smp
