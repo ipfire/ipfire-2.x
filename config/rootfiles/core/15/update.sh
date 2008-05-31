9ece7e6e0affd5ebe82c5f012e513a3310bdcf9e
Arne Fitzenreiter <arne_f@ipfire.org>
2008-05-31 11:12:03 +0200

    Add display fixes if not using red0 to core15

--- config/rootfiles/core/15/update.sh
+++ config/rootfiles/core/15/update.sh
@@ -3,4 +3,7 @@
 /usr/local/bin/backupctrl exclude >/dev/null 2>&1
 extract_files
 perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
+grep -v 'Interface "ppp"' /etc/collectd.conf > /tmp/collectd.conf
+mv /tmp/collectd.conf /etc/collectd.conf
+/etc/init.d/collectd restart
 /etc/init.d/mISDN config
