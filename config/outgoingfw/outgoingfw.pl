52802b5fd8beb68a6a6cb671a511d3e108a1ebac
Maniacikarus <maniacikarus@ipfire.org>
2008-06-23 18:46:42 +0200

    Fixed Outgoing FW ignoring Log Option in Mode1

--- config/outgoingfw/outgoingfw.pl
+++ config/outgoingfw/outgoingfw.pl
@@ -213,6 +213,15 @@ if ($P2PSTRING) {
 }
 
 if ( $outfwsettings{'POLICY'} eq 'MODE1' ) {
+	 if ( $outfwsettings{'MODE1LOG'} eq 'on' ) {
+		 	$CMD = "/sbin/iptables -A OUTGOINGFW -o $netsettings{'RED_DEV'} -m limit --limit 10/minute -j LOG --log-prefix 'OUTGOINGFW '";
+		if ($DEBUG) {
+			print "$CMD\n";
+		} else {
+			system("$CMD");
+		}
+	 }
+
 	$CMD = "/sbin/iptables -A OUTGOINGFW -o $netsettings{'RED_DEV'} -j DROP -m comment --comment 'DROP_OUTGOINGFW'";
 	if ($DEBUG) {
 		print "$CMD\n";
