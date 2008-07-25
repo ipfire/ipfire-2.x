683beb54de28a5428d34bcd0bd20f7db21ad3200
Arne Fitzenreiter <arne_f@ipfire.org>
2008-07-25 11:40:02 +0200

    Marked as beta2 fix meta-file of updater

--- make.sh
+++ make.sh
@@ -24,7 +24,7 @@
 
 NAME="IPFire"										# Software name
 SNAME="ipfire"									# Short name
-VERSION="2.3-test"
+VERSION="2.3-beta2"
 GIT_BRANCH=master:master										# Version number
 SLOGAN="www.ipfire.org"					# Software slogan
 CONFIG_ROOT=/var/ipfire					# Configuration rootdir
