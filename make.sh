4e464f98f0871c06058cfe1dacce120308c3af51
Michael Tremer <michael.tremer@ipfire.org>
2008-09-08 19:38:39 +0000

    Added apcupsd.

--- make.sh
+++ make.sh
@@ -588,6 +588,7 @@ buildipfire() {
   ipfiremake dbus
   ipfiremake bluetooth
   ipfiremake gutenprint
+  ipfiremake apcupsd
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
