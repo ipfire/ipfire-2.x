fe4c98d3cb792b19038b6a20b283da7bcfbc62cb
Arne Fitzenreiter <arne_f@ipfire.org>
2008-11-01 21:07:06 +0100

    Fix lynis rootfile

--- tools/checknewlog.pl
+++ tools/checknewlog.pl
@@ -28,7 +28,7 @@ foreach(@FILES) {
 #	print $_."\n";
 	my $Found = 0;
 
-	if ( $_ =~ /$\.log/ || $_ =~ /^\.+/  || $_=~ /-install/ || $_=~ /-tools/ || $_=~ /-config/ || $_=~ /-kmod-/){
+	if ( $_ =~ /$\.log/ || $_ =~ /^\.+/  || $_=~ /-install/ || $_=~ /-tools/ || $_=~ /-config/ || $_=~ /-kmod-/ || $_ eq 'FILES' ){
 		next;
 	} elsif ( $_=~ /missing_rootfile/ ){
 		print "Rootfile for $_ missing!\n";
