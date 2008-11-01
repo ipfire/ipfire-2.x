1aa810ea14f540622b65056736be81db16ec12f6
Maniacikarus <maniacikarus@ipfire.org>
2008-11-01 14:51:14 +0100

    Upgraded packs and make.sh

--- make.sh
+++ make.sh
@@ -580,6 +580,7 @@ buildipfire() {
   ipfiremake iperf
   ipfiremake netcat
   ipfiremake 7zip
+  ipfiremake lynis
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
