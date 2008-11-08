e3d6034aca0946a95c06cee004c7229af994b0fe
Maniacikarus <maniacikarus@ipfire.org>
2008-11-08 21:13:08 +0100

    Added cryptsetup addon

--- make.sh
+++ make.sh
@@ -581,6 +581,7 @@ buildipfire() {
   ipfiremake netcat
   ipfiremake 7zip
   ipfiremake lynis
+  ipfiremake cryptsetup
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
