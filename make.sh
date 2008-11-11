050080d67b5bf439ec6e39be0d4fa8f5ea311b36
Michael Tremer <michael.tremer@ipfire.org>
2008-11-11 22:21:04 +0100

    Added splix - printing for samsung laser printers.

--- make.sh
+++ make.sh
@@ -582,6 +582,7 @@ buildipfire() {
   ipfiremake 7zip
   ipfiremake lynis
   ipfiremake cryptsetup
+  ipfiremake splix
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
