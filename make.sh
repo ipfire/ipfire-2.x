c268aebd1cbcf3844b0d6ca91c89c3236430221f
Arne Fitzenreiter <arne_f@ipfire.org>
2008-11-13 14:33:33 +0100

    Add streamripper package

--- make.sh
+++ make.sh
@@ -583,6 +583,7 @@ buildipfire() {
   ipfiremake lynis
   ipfiremake cryptsetup
   ipfiremake splix
+  ipfiremake streamripper
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
