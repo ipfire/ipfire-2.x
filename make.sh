d0515b01f7ced53d877109d1ad7fb5edac78cda2
maniacikarus <maniacikarus@ipfire.org>
2009-01-25 11:19:06 +0100

    Added new Addon sslh Deamon for ssh/ssl proxy on red

--- make.sh
+++ make.sh
@@ -590,6 +590,7 @@ buildipfire() {
   ipfiremake sqlite
   ipfiremake taglib
   ipfiremake mediatomb
+  ipfiremake sslh
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
