f6f00a6b4044803dedf46a4ee0a074678ecabba7
maniacikarus <maniacikarus@ipfire.org>
2009-02-09 18:11:05 +0100

    Started core 27

--- config/rootfiles/core/27/update.sh
+++ config/rootfiles/core/27/update.sh
@@ -2,5 +2,6 @@
 . /opt/pakfire/lib/functions.sh
 /usr/local/bin/backupctrl exclude >/dev/null 2>&1
 extract_files
-#perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
+perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
 sysctl -p
+/etc/init.d/squid restart
