b19300441b4da91290dd69be7dfcce9814acb4df
maniacikarus <maniacikarus@ipfire.org>
2009-02-19 23:02:52 +0100

    Fixed core errors

--- config/rootfiles/core/27/update.sh
+++ config/rootfiles/core/27/update.sh
@@ -3,9 +3,9 @@
 /usr/local/bin/backupctrl exclude >/dev/null 2>&1
 /etc/init.d/squid stop
 extract_files
-[ -e /var/ipfire/qos/enable ] && qosctrl stop
-qosctrl generate
-[ -e /var/ipfire/qos/enable ] && qosctrl start
+[ -e /var/ipfire/qos/enable ] && /usr/local/bin/qosctrl stop
+/usr/local/bin/qosctrl generate
+[ -e /var/ipfire/qos/enable ] && /usr/local/bin/qosctrl start
 perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
 sysctl -p
 mv /etc/squid/cachemgr.conf /var/ipfire/proxy/cachemgr.conf
