cc3f6e5c010aad9759fa48b9867077e969c5e0e9
root <root@ipfire.localdomain>
2009-04-05 11:14:47 +0200

    Perl-module DBI added

--- make.sh
+++ make.sh
@@ -594,6 +594,7 @@ buildipfire() {
   ipfiremake vdradmin
   ipfiremake miau
   ipfiremake net-snmp
+  ipfiremake perl-DBI
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
