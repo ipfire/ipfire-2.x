e8b7789107108bedec9d62fa67b337983054fa39
Arne Fitzenreiter <arne_f@ipfire.org>
2009-05-02 23:09:17 +0200

    Set version to 2.5beta2

--- make.sh
+++ make.sh
@@ -24,7 +24,7 @@
 
 NAME="IPFire"										# Software name
 SNAME="ipfire"									# Short name
-VERSION="2.5beta1"
+VERSION="2.5beta2"
 CORE="28"
 GIT_BRANCH=master:master										# Version number
 SLOGAN="www.ipfire.org"					# Software slogan
