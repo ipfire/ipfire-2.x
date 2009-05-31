91d539abbcf45201909d4f4cbfdeae2225a967cb
Peter Pfeiffer <peterman@ipfire.org>
2009-05-31 09:13:22 +0200

    add NagiosQL for Webconfig Nagios

--- make.sh
+++ make.sh
@@ -614,6 +614,7 @@ buildipfire() {
   ipfiremake lcr
   ipfiremake cacti
   ipfiremake open-vm-tools
+  ipfiremake nagiosql
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
