5912ab0ada866c88348b0629f49f8b2537c56306
Arne Fitzenreiter <arne_f@ipfire.org>
2009-08-04 19:16:00 +0200

    Started core30.

--- make.sh
+++ make.sh
@@ -25,7 +25,7 @@
 NAME="IPFire"										# Software name
 SNAME="ipfire"									# Short name
 VERSION="2.5"
-CORE="29"
+CORE="30"
 GIT_BRANCH=master:master										# Version number
 SLOGAN="www.ipfire.org"					# Software slogan
 CONFIG_ROOT=/var/ipfire					# Configuration rootdir
