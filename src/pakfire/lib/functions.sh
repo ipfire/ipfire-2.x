521a8b45e28092dce1d2d23bf5137957b482d2ab
Arne Fitzenreiter <arne_f@ipfire.org>
2010-03-28 16:11:48 +0200

    Fix permission change of existing directories at core updates.

--- src/pakfire/lib/functions.sh
+++ src/pakfire/lib/functions.sh
@@ -24,7 +24,7 @@
 
 extract_files() {
 	echo "Extracting files..."
-	tar xvf /opt/pakfire/tmp/files -p --numeric-owner -C /
+	tar xvf /opt/pakfire/tmp/files --no-overwrite-dir -p --numeric-owner -C /
 	echo "...Finished."
 }
 
