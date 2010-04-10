0e067ff3baffcaf9840982aa46cacfe7508a782f
Arne Fitzenreiter <arne_f@ipfire.org>
2010-04-10 08:20:52 +0200

    Updated Kernel (2.6.32.11).

--- config/rootfiles/core/38/update.sh
+++ config/rootfiles/core/38/update.sh
@@ -24,7 +24,7 @@
 . /opt/pakfire/lib/functions.sh
 /usr/local/bin/backupctrl exclude >/dev/null 2>&1
 #
-KVER="2.6.32.10"
+KVER="2.6.32.11"
 ROOT=`grep "root=" /boot/grub/grub.conf | cut -d"=" -f2 | cut -d" " -f1 | tail -n 1`
 MOUNT=`grep "kernel" /boot/grub/grub.conf | tail -n 1`
 # Nur den letzten Parameter verwenden
