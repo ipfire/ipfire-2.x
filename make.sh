69e29b22deca7d82f1cc21202fdb7e101bcecb62
Arne Fitzenreiter <arne_f@ipfire.org>
2010-07-12 00:24:26 +0200

    Add ddrescue package.

--- make.sh
+++ make.sh
@@ -641,6 +641,7 @@ buildipfire() {
   ipfiremake eject
   ipfiremake pound
   ipfiremake minicom
+  ipfiremake ddrescue
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
