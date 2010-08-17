b0ab1b098ed35da40aebc5bb3ab77839d9f17a53
Arne Fitzenreiter <arne_f@ipfire.org>
2010-08-18 00:01:50 +0200

    Start updater (test).

---
+++ config/rootfiles/core/test/update.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+############################################################################
+#                                                                          #
+# This file is part of the IPFire Firewall.                                #
+#                                                                          #
+# IPFire is free software; you can redistribute it and/or modify           #
+# it under the terms of the GNU General Public License as published by     #
+# the Free Software Foundation; either version 3 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# IPFire is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+# GNU General Public License for more details.                             #
+#                                                                          #
+# You should have received a copy of the GNU General Public License        #
+# along with IPFire; if not, write to the Free Software                    #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA #
+#                                                                          #
+# Copyright (C) 2010 IPFire-Team <info@ipfire.org>.                        #
+#                                                                          #
+############################################################################
+#
+. /opt/pakfire/lib/functions.sh
+/usr/local/bin/backupctrl exclude >/dev/null 2>&1
+#
+KVER="2.6.32.19"
+ROOT=`grep "root=" /boot/grub/grub.conf | cut -d"=" -f2 | cut -d" " -f1 | tail -n 1`
+MOUNT=`grep "kernel" /boot/grub/grub.conf | tail -n 1`
+# Nur den letzten Parameter verwenden
+echo $MOUNT > /dev/null
+MOUNT=$_
+if [ ! $MOUNT == "rw" ]; then
+	MOUNT="ro"
+fi
+#
+# check if we the backup file already exist
+if [ -e /var/ipfire/backup/core-upgrade_$KVER.tar.bz2 ]; then
+    echo Moving backup to backup-old ...
+    mv -f /var/ipfire/backup/core-upgrade_$KVER.tar.bz2 \
+       /var/ipfire/backup/core-upgrade_$KVER-old.tar.bz2
+fi
+echo First we made a backup of all files that was inside of the
+echo update archive. This may take a while ...
+# Add some files that are not in the package to backup
+echo lib/modules >> /opt/pakfire/tmp/ROOTFILES
+echo boot >> /opt/pakfire/tmp/ROOTFILES
+echo etc/mkinitcpio.conf >> /opt/pakfire/tmp/ROOTFILES
+echo etc/mkinitcpio.conf.org >> /opt/pakfire/tmp/ROOTFILES
+echo etc/mkinitcpio.d >> /opt/pakfire/tmp/ROOTFILES
+echo lib/initcpio >> /opt/pakfire/tmp/ROOTFILES
+echo sbin/mkinitcpio >> /opt/pakfire/tmp/ROOTFILES
+
+# Backup the files
+tar cjvf /var/ipfire/backup/core-upgrade_$KVER.tar.bz2 \
+    -C / -T /opt/pakfire/tmp/ROOTFILES --exclude='#*' > /dev/null 2>&1
+
+echo
+echo Update Kernel to $KVER ...
+# Remove old kernel, configs, initrd, modules ...
+#
+rm -rf /boot/System.map-*
+rm -rf /boot/config-*
+rm -rf /boot/ipfirerd-*
+rm -rf /boot/vmlinuz-*
+rm -rf /lib/modules/*-ipfire
+# Remove mkinitcpio
+rm -rf /etc/mkinitcpio.*
+rm -rf /lib/initcpio
+rm -rf /sbin/mkinitcpio
+#
+# Backup grub.conf
+#
+cp -vf /boot/grub/grub.conf /boot/grub/grub.conf.org
+#
+# Unpack the updated files
+#
+echo
+echo Unpack the updated files ...
+#
+tar xvf /opt/pakfire/tmp/files --preserve --numeric-owner -C / \
+	--no-overwrite-dir
+#
+# Modify grub.conf
+#
+echo
+echo Update grub configuration ...
+sed -i "s|ROOT|$ROOT|g" /boot/grub/grub.conf
+sed -i "s|KVER|$KVER|g" /boot/grub/grub.conf
+sed -i "s|MOUNT|$MOUNT|g" /boot/grub/grub.conf
+
+if [ "$(grep "^serial" /boot/grub/grub.conf.org)" == "" ]; then
+	echo "grub use default console ..."
+else
+	echo "grub use serial console ..."
+	sed -i -e "s|splashimage|#splashimage|g" /boot/grub/grub.conf
+	sed -i -e "s|#serial|serial|g" /boot/grub/grub.conf
+	sed -i -e "s|#terminal|terminal|g" /boot/grub/grub.conf
+	sed -i -e "s| panic=10 | console=ttyS0,38400n8 panic=10 |g" /boot/grub/grub.conf
+fi
+#
+# ReInstall grub
+#
+grub-install --no-floppy ${ROOT::`expr length $ROOT`-1} --recheck
+#
+# Rebuild Language
+#
+#perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
+#
+# Delete old lm-sensor modullist to force search at next boot
+#
+rm -rf /etc/sysconfig/lm_sensors
+/usr/bin/logger -p syslog.emerg -t kernel "Upgrade finished. If you use a customized grub.cfg"
+/usr/bin/logger -p syslog.emerg -t kernel "Check it before reboot !!!"
+/usr/bin/logger -p syslog.emerg -t kernel " *** Please reboot... *** "
