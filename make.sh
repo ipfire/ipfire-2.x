aac0f7118f0630a1987324c4d29c642be06a9f94
Christian Schmidt <christian.schmidt@ipfire.org>
2010-08-28 08:38:10 +0200

    First integration of imspector, some more work to do.

--- make.sh
+++ make.sh
@@ -643,6 +643,7 @@ buildipfire() {
   ipfiremake pound
   ipfiremake minicom
   ipfiremake ddrescue
+  ipfiremake imspector
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
