e9581f09df723395dc0c56bd88c35faa7f43d311
Michael Tremer <michael.tremer@ipfire.org>
2010-11-22 01:12:05 +0100

    New package: miniupnpd.

--- make.sh
+++ make.sh
@@ -648,6 +648,7 @@ buildipfire() {
   ipfiremake minicom
   ipfiremake ddrescue
   ipfiremake imspector
+  ipfiremake miniupnpd
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
