19947356c852c5dd92460cf115e448a0cb14f3bc
Michael Tremer <michael.tremer@ipfire.org>
2010-12-24 01:18:36 +0100

    Fix grammar/typo in last commit.

--- src/install+setup/setup/main.c
+++ src/install+setup/setup/main.c
@@ -202,7 +202,7 @@ EXIT:
 		{
 			newtWinMessage(ctr[TR_WARNING], ctr[TR_OK], ctr[TR_SETUP_NOT_COMPLETE]);
 
-			fprintf(flog, "Setup program not finnished.\n");
+			fprintf(flog, "Setup has program not finished.\n");
 			fflush(flog);
 			fclose(flog);
 
