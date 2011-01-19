5d547072c3e454549eb0ac0c36827ebcba8eba4a
Michael Tremer <michael.tremer@ipfire.org>
2011-01-19 01:27:23 +0100

    parted: New package.

--- make.sh
+++ make.sh
@@ -685,6 +685,7 @@ buildipfire() {
   ipfiremake miniupnpd
   ipfiremake client175
   ipfiremake powertop
+  ipfiremake parted
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
