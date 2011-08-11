c02c8de996cc92660fb4292ab5069fe4dedecd06
Arne Fitzenreiter <arne_f@ipfire.org>
2011-08-11 18:50:00 +0200

    core44: remove also the xen modules.
    if old modules are not removed the ct-server root run out of space.

--- config/rootfiles/core/44/update.sh
+++ config/rootfiles/core/44/update.sh
@@ -68,7 +68,7 @@ rm -rf /boot/System.map-*
 rm -rf /boot/config-*
 rm -rf /boot/ipfirerd-*
 rm -rf /boot/vmlinuz-*
-rm -rf /lib/modules/*-ipfire
+rm -rf /lib/modules/*
 # Remove mkinitcpio
 rm -rf /etc/mkinitcpio.*
 rm -rf /lib/initcpio
