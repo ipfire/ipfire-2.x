b0dcbdeae448d3fc852c9ee9a4e5e6b9ab75fd34
Arne Fitzenreiter <arne_f@ipfire.org>
2011-09-11 16:06:52 +0200

    core53: next is core53 now. set version in backupiso and pakfire to 2.11.

--- config/rootfiles/core/53/update.sh
+++ config/rootfiles/core/53/update.sh
@@ -26,7 +26,7 @@
 
 #
 # Remove old core updates from pakfire cache to save space...
-core=53
+core=52
 for (( i=1; i<=$core; i++ ))
 do
 	rm -f /var/cache/pakfire/core-upgrade-*-$i.ipfire
@@ -34,26 +34,74 @@ done
 
 #
 #Stop services
-/etc/init.d/squid stop
-/etc/init.d/apache stop
+
+/etc/init.d/ipsec stop
+
+#
+# Remove old strongswan libs
+rm -rf /usr/lib/libcharon.so
+rm -rf /usr/lib/libcharon.so.0
+rm -rf /usr/lib/libcharon.so.0.0.0
+rm -rf /usr/lib/libhydra.so
+rm -rf /usr/lib/libhydra.so.0
+rm -rf /usr/lib/libhydra.so.0.0.0
+rm -rf /usr/lib/libstrongswan.so
+rm -rf /usr/lib/libstrongswan.so.0
+rm -rf /usr/lib/libstrongswan.so.0.0.0
+rm -rf /usr/libexec/ipsec/plugins
+
+# Remove old usb_modeswitch_data
+rm -rf /etc/usb_modeswitch.d
 
 #
 #Extract files
 extract_files
 
 #
+#Replace now unsupported EU regdomain by a "DE" comment.
+sed -i -e "s|^options cfg80211 ieee80211_regdom=EU|#options cfg80211 ieee80211_regdom=DE|g" \
+		/etc/modprobe.d/cfg80211
+
+#
 #Start services
-/etc/init.d/apache start
-/etc/init.d/squid start
+
+if [ `grep "ENABLED=on" /var/ipfire/vpn/settings` ]; then
+	/etc/init.d/ipsec start
+fi
 
 #
 #Update Language cache
 perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
 
 #Rebuild module dep's
-#depmod 2.6.32.43-ipfire     >/dev/null 2>&1
-#depmod 2.6.32.43-ipfire-pae >/dev/null 2>&1
-#depmod 2.6.32.43-ipfire-xen >/dev/null 2>&1
+depmod 2.6.32.45-ipfire     >/dev/null 2>&1
+depmod 2.6.32.45-ipfire-pae >/dev/null 2>&1
+depmod 2.6.32.45-ipfire-xen >/dev/null 2>&1
+
+#
+# After pakfire has ended run it again and update the lists and do upgrade
+#
+echo '#!/bin/bash'                                        >  /tmp/pak_update
+echo 'while [ "$(ps -A | grep " update.sh")" != "" ]; do' >> /tmp/pak_update
+echo '    sleep 1'                                        >> /tmp/pak_update
+echo 'done'                                               >> /tmp/pak_update
+echo 'while [ "$(ps -A | grep " pakfire")" != "" ]; do'   >> /tmp/pak_update
+echo '    sleep 1'                                        >> /tmp/pak_update
+echo 'done'                                               >> /tmp/pak_update
+echo '/opt/pakfire/pakfire update -y --force'             >> /tmp/pak_update
+echo '/opt/pakfire/pakfire upgrade -y'                    >> /tmp/pak_update
+echo '/opt/pakfire/pakfire upgrade -y'                    >> /tmp/pak_update
+echo '/opt/pakfire/pakfire upgrade -y'                    >> /tmp/pak_update
+echo 'sync && sync && sync'                               >> /tmp/pak_update
+echo '/usr/bin/logger -p syslog.emerg -t core-upgrade-53 " *** Please reboot... *** "' >> /tmp/pak_update
+echo 'touch /var/run/need_reboot ' >> /tmp/pak_update
+
+#
+chmod +x /tmp/pak_update
+/tmp/pak_update &
+echo
+echo Please wait until pakfire has ended...
+echo
 
 #
 #Finish
