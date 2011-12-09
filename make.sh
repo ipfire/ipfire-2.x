3f18f99624664607023ef4ba505c281faa3a48e8
Arne Fitzenreiter <arne_f@ipfire.org>
2011-12-09 19:34:14 +0100

    start core55.

--- make.sh
+++ make.sh
@@ -25,7 +25,7 @@
 NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 VERSION="2.11"							# Version number
-CORE="54"							# Core Level (Filename)
+CORE="55"							# Core Level (Filename)
 PAKFIRE_CORE="54"						# Core Level (PAKFIRE)
 GIT_BRANCH=`git status | head -n1 | cut -d" " -f4`		# Git Branch
 SLOGAN="www.ipfire.org"						# Software slogan
