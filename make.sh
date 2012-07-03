2e6c5015ec964a551ceb214052744effe1facb61
Michael Tremer <michael.tremer@ipfire.org>
2012-07-03 16:21:53 +0200

    Add lcdproc to make.sh.

--- make.sh
+++ make.sh
@@ -770,6 +770,7 @@ buildipfire() {
   ipfiremake sysbench
   ipfiremake strace
   ipfiremake ipfire-netboot
+  ipfiremake lcdproc
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
