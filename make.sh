9d0acf61e017f010df9f77107da45825a7f3f3dc
Erik Kapfer <erik.kapfer@ipfire.org>
2012-08-24 15:44:11 +0200

    fping: New package.

--- make.sh
+++ make.sh
@@ -770,6 +770,7 @@ buildipfire() {
   ipfiremake mtr
   ipfiremake tcpick
   ipfiremake minidlna
+  ipfiremake fping
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
