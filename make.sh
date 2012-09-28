2cb35915849558b46ecc492a2c646057643b9945
Arne Fitzenreiter <arne_f@ipfire.org>
2012-09-29 01:14:46 +0200

    linux-firmware: update to 236367d.

--- make.sh
+++ make.sh
@@ -374,7 +374,6 @@ buildipfire() {
   ipfiremake linux-firmware
   ipfiremake zd1211-firmware
   ipfiremake rpi-firmware
-  ipfiremake fw_ath9k_htc
   ipfiremake u-boot
 
   # The xen and PAE kernels are only available for x86
