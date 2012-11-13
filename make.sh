cc8ac76307ebb11b010dd6cbc83ad14c0c0c3d83
Erik Kapfer <erik.kapfer@ipfire.org>
2012-11-13 22:31:39 +0100

    stress: New package.

--- make.sh
+++ make.sh
@@ -772,6 +772,7 @@ buildipfire() {
   ipfiremake minidlna
   ipfiremake fping
   ipfiremake telnet
+  ipfiremake stress
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
