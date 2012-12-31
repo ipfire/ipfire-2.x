f98766eb9eac8dee2cfcbdabb78755e08ec39418
Erik Kapfer <erik.kapfer@ipfire.org>
2012-12-31 16:21:01 +0100

    sendEmail: New addon
    Fixes #10242

--- make.sh
+++ make.sh
@@ -561,6 +561,7 @@ buildipfire() {
   ipfiremake Net-IPv4Addr
   ipfiremake Net_SSLeay
   ipfiremake IO-Stringy
+  ipfiremake IO-Socket-SSL
   ipfiremake Unix-Syslog
   ipfiremake Mail-Tools
   ipfiremake MIME-Tools
@@ -775,6 +776,7 @@ buildipfire() {
   ipfiremake stress
   ipfiremake libstatgrab
   ipfiremake sarg
+  ipfiremake sendEmail
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
