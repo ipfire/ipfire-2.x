3ab685b34bd439f77c2f90f4c8214b025eb99e1c
Arne Fitzenreiter <arne_f@ipfire.org>
2013-02-14 22:16:16 +0100

    IPFire 2.13 final.

--- make.sh
+++ make.sh
@@ -24,9 +24,9 @@
 
 NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
-VERSION="2.13rc2"							# Version number
-CORE="65"							# Core Level (Filename)
-PAKFIRE_CORE="65"						# Core Level (PAKFIRE)
+VERSION="2.13"							# Version number
+CORE="66"							# Core Level (Filename)
+PAKFIRE_CORE="66"						# Core Level (PAKFIRE)
 GIT_BRANCH=`git status | head -n1 | cut -d" " -f4`		# Git Branch
 SLOGAN="www.ipfire.org"						# Software slogan
 CONFIG_ROOT=/var/ipfire						# Configuration rootdir
