c43ae64f998b82200c6249c002b4d08503c50787
Joern-Ingo Weigert <jiweigert@gmail.com>
2013-03-04 22:04:34 +0100

    Enable private caching

--- config/cfgroot/header.pl
+++ config/cfgroot/header.pl
@@ -161,8 +161,7 @@ sub genmenu {
 
 sub showhttpheaders
 {
-	print "Pragma: no-cache\n";
-	print "Cache-control: no-cache\n";
+	print "Cache-control: private\n";
 	print "Connection: close\n";
 	print "Content-type: text/html\n\n";
 }
