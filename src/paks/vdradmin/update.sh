5386d69ca03900fbd0218509b04874dd44592cd1
Michael Tremer <michael.tremer@ipfire.org>
2013-03-27 13:04:32 +0100

    vdradmin: Update configuration for new versions of VDR.

---
+++ src/paks/vdradmin/update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+############################################################################
+#                                                                          #
+# This file is part of the IPFire Firewall.                                #
+#                                                                          #
+# IPFire is free software; you can redistribute it and/or modify           #
+# it under the terms of the GNU General Public License as published by     #
+# the Free Software Foundation; either version 2 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# IPFire is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+# GNU General Public License for more details.                             #
+#                                                                          #
+# You should have received a copy of the GNU General Public License        #
+# along with IPFire; if not, write to the Free Software                    #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA #
+#                                                                          #
+# Copyright (C) 2007 IPFire-Team <info@ipfire.org>.                        #
+#                                                                          #
+############################################################################
+#
+. /opt/pakfire/lib/functions.sh
+
+./uninstall.sh
+./install.sh
+
+# Update configuration options for new versions of VDR.
+stop_service ${NAME}
+
+sed -i /etc/vdradmin/vdradmind.conf \
+	-e "s/^LANG = .*/LANG = en_US.utf8/g" \
+	-e "s/^PORT = .*/PORT = 6419/g"
+
+start_service ${NAME}
