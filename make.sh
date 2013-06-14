3142f133bba44b44336bdd0c48ad0d859522964d
Michael Tremer <michael.tremer@ipfire.org>
2013-06-14 13:34:49 +0200

    New package: keepalived

--- make.sh
+++ make.sh
@@ -770,6 +770,7 @@ buildipfire() {
   ipfiremake lcdproc
   ipfiremake bitstream
   ipfiremake multicat
+  ipfiremake keepalived
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
