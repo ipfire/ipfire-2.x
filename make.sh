85981472c3ef260141178afe9a006935ecb913e2
Jan Lentfer <Jan.Lentfer@ipfire.org>
2013-06-17 11:05:59 +0200

    new addon: swatch - Simple log file watcher
    http://sourceforge.net/projects/swatch/
    
    With swatch you can easily monitor (growing) log files
    in realtime and create email alerts based on log file content.
    
    e.g. with a config file like this:
    watchfor /Priority\: ([1|2])/
    echo=normal
    mail=alerts@your.domain,subject=[SNORT] Priority $1 Alert
    
    and a swatch command like this:
    swatch --daemon -c /var/ipfire/snort/swatchrc --input-record-separator='\n\n' -t /var/log/snort/alert
    
    you can setup email alerts for SNORT alerts.
    
    This still needs an active MTA (e.g. dma or postfix).

--- make.sh
+++ make.sh
@@ -768,6 +768,12 @@ buildipfire() {
   ipfiremake strace
   ipfiremake ipfire-netboot
   ipfiremake lcdproc
+  ipfiremake perl-Carp-Clan
+  ipfiremake perl-Date-Calc
+  ipfiremake perl-Date-Manip
+  ipfiremake perl-File-Tail
+  ipfiremake perl-TimeDate
+  ipfiremake swatch
   echo Build on $HOSTNAME > $BASEDIR/build/var/ipfire/firebuild
   cat /proc/version >> $BASEDIR/build/var/ipfire/firebuild
   echo >> $BASEDIR/build/var/ipfire/firebuild
