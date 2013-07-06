e2a9e21f68c848af352330b2fc9af63daaf6c5aa
Arne Fitzenreiter <arne_f@ipfire.org>
2013-07-06 17:00:40 +0200

    core71: add wlanclient to updater.

--- config/rootfiles/core/71/update.sh
+++ config/rootfiles/core/71/update.sh
@@ -44,7 +44,7 @@ extract_files
 
 #
 #Update Language cache
-#perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
+perl -e "require '/var/ipfire/lang.pl'; &Lang::BuildCacheLang"
 
 sync
 
