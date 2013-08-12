8c60701a4f856689e5bfae2ff2b6c5b7c0f0fdad
Michael Tremer <michael.tremer@ipfire.org>
2013-08-12 14:42:16 +0200

    forwardctrl: Remove unused and possibly dangerous flush option.
    Also remove unused header files.

--- src/misc-progs/forwardfwctrl.c
+++ src/misc-progs/forwardfwctrl.c
@@ -5,23 +5,12 @@
  *
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include "setuid.h"
 
 int main(int argc, char *argv[]) {
-
 	if (!(initsetuid()))
 		exit(1);
 
-	if(argc > 1)
-		safe_system("/var/ipfire/forward/bin/rules.pl flush");
-	else
-		safe_system("/var/ipfire/forward/bin/rules.pl");
-	
+	safe_system("/var/ipfire/forward/bin/rules.pl");
 	return 0;
 }
