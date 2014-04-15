f1f1114a8bd7a24a6d62c9ff47b75be29aef4dcd
Michael Tremer <michael.tremer@ipfire.org>
2014-04-15 21:25:24 +0200

    Build Core Update 76.

--- make.sh
+++ make.sh
@@ -25,8 +25,8 @@
 NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 VERSION="2.13"							# Version number
-CORE="75"							# Core Level (Filename)
-PAKFIRE_CORE="75"						# Core Level (PAKFIRE)
+CORE="76"							# Core Level (Filename)
+PAKFIRE_CORE="76"						# Core Level (PAKFIRE)
 GIT_BRANCH=`git status | head -n1 | cut -d" " -f4`		# Git Branch
 SLOGAN="www.ipfire.org"						# Software slogan
 CONFIG_ROOT=/var/ipfire						# Configuration rootdir
