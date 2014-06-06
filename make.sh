5f3f94821f287dac013274b4a2bc04cfb5ee74f0
Arne Fitzenreiter <arne_f@ipfire.org>
2014-06-06 09:55:51 +0200

    core78: create emergency update.
    all changes planned and build in testing will moved to core79.

--- make.sh
+++ make.sh
@@ -25,8 +25,8 @@
 NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 VERSION="2.15"							# Version number
-CORE="77"							# Core Level (Filename)
-PAKFIRE_CORE="77"						# Core Level (PAKFIRE)
+CORE="78"							# Core Level (Filename)
+PAKFIRE_CORE="78"						# Core Level (PAKFIRE)
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`			# Git Branch
 SLOGAN="www.ipfire.org"						# Software slogan
 CONFIG_ROOT=/var/ipfire						# Configuration rootdir
