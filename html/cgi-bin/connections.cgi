ipfire/ipfire-2.x

commit e60cd3a4042e95cf2748aac2de9d991c724ef24d

use colour of destination network for DNAT

html/cgi-bin/connections.cgi
@@ -520,7 +520,8 @@ foreach my $line (@conntrack) {
 	}
 
 	my $sip_colour = ipcolour($sip);
-	my $dip_colour = ipcolour($dip);
+	# use colour of destination network for DNAT
+	my $dip_colour = $dip ne $dip_ret ? ipcolour($dip_ret) : ipcolour($dip);
 
 	my $sserv = '';
 	if ($sport < 1024) {

commit ee299e2ecf6e9fbc6dbf7fdc18c5a8980e122b12

connections.cgi: Fix broken NAT rules when there is an empty destination IP address
Bug introduced with e60cd3a4042e95cf2748aac2de9d991c724ef24d

Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>
Reported-by: Daniel Weismüller <daniel.weismueller@ipfire.org>

html/cgi-bin/connections.cgi
@@ -540,7 +540,7 @@ foreach my $line (@conntrack) {
 	$ttl = format_time($ttl);
 
 	my $sip_extra;
-	if ($sip ne $sip_ret) {
+	if ($sip_ret && $sip ne $sip_ret) {
 		$sip_extra = "<span style='color:#FFFFFF;'>&gt;</span> ";
 		$sip_extra .= "<a href='/cgi-bin/ipinfo.cgi?ip=$sip_ret'>";
 		$sip_extra .= "	<span style='color:#FFFFFF;'>$sip_ret</span>";
@@ -548,7 +548,7 @@ foreach my $line (@conntrack) {
 	}
 
 	my $dip_extra;
-	if ($dip ne $dip_ret) {
+	if ($dip_ret && $dip ne $dip_ret) {
 		$dip_extra = "<span style='color:#FFFFFF;'>&gt;</span> ";
 		$dip_extra .= "<a href='/cgi-bin/ipinfo.cgi?ip=$dip_ret'>";
 		$dip_extra .= " <span style='color:#FFFFFF;'>$dip_ret</span>";
@@ -661,15 +661,17 @@ sub ipcolour($) {
 	my ($ip) = $_[0];
 	my $found = 0;
 
-	foreach my $line (@network) {
-		if ($network[$id] eq '') {
-			$id++;
-		} else {
-			if (!$found && ipv4_in_network($network[$id], $masklen[$id], $ip) ) {
-				$found = 1;
-				$colour = $colour[$id];
+	if ($ip) {
+		foreach my $line (@network) {
+			if ($network[$id] eq '') {
+				$id++;
+			} else {
+				if (!$found && ipv4_in_network($network[$id], $masklen[$id], $ip) ) {
+					$found = 1;
+					$colour = $colour[$id];
+				}
+				$id++;
 			}
-			$id++;
 		}
 	}
 