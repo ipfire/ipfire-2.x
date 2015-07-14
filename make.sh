5b64ed2e3634be2d500120976d7175178da4440c
Michael Tremer <michael.tremer@ipfire.org>
2015-07-14 16:58:13 +0200

    Start Core Update 93
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- make.sh
+++ make.sh
@@ -25,7 +25,7 @@
 NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 VERSION="2.17"							# Version number
-CORE="92"							# Core Level (Filename)
+CORE="93"							# Core Level (Filename)
 PAKFIRE_CORE="92"						# Core Level (PAKFIRE)
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`			# Git Branch
 SLOGAN="www.ipfire.org"						# Software slogan
