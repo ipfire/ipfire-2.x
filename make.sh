9a6be065359cc1166af4caf61c0e3e1419cbc2d2
Arne Fitzenreiter <arne_f@ipfire.org>
2016-04-06 19:57:46 +0200

    core101: start update.
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- make.sh
+++ make.sh
@@ -25,7 +25,7 @@
 NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 VERSION="2.19"							# Version number
-CORE="100"							# Core Level (Filename)
+CORE="101"							# Core Level (Filename)
 PAKFIRE_CORE="100"						# Core Level (PAKFIRE)
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`			# Git Branch
 SLOGAN="www.ipfire.org"						# Software slogan
