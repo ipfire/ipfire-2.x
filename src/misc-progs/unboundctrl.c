df7340d2f3232a87ae6e3e11a6cb4e15b74e55a3
Michael Tremer <michael.tremer@ipfire.org>
2016-08-06 19:32:34 +0100

    Add unboundctrl
    Control binary to relaunch unbound from the web user interface
    
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

---
+++ src/misc-progs/unboundctrl.c
@@ -0,0 +1,34 @@
+/* This file is part of the IPFire Firewall.
+ *
+ * This program is distributed under the terms of the GNU General Public
+ * Licence.  See the file COPYING for details.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include "setuid.h"
+
+int main(int argc, char *argv[]) {
+
+	if (!(initsetuid()))
+		exit(1);
+
+	if (argc < 2) {
+		fprintf(stderr, "\nNo argument given.\n\nunboundctrl (restart)\n\n");
+		exit(1);
+	}
+
+	if (strcmp(argv[1], "restart") == 0) {
+		safe_system("/etc/rc.d/init.d/unbound restart");
+	} else {
+		fprintf(stderr, "\nBad argument given.\n\nunboundctrl (restart)\n\n");
+		exit(1);
+	}
+
+	return 0;
+}
