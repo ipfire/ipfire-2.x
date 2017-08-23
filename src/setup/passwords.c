d41fe99f74689075967f027f37ff9a2d7a09339b
Wolfgang Apolinarski <wolfgang.apolinarski@googlemail.com>
2017-08-23 21:25:25 +0200

    Update to apache 2.4.27
    - Updated to apache 2.4
    - Updated the htpasswd generation to use the more secure bcrypt algorithm
    
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- src/setup/passwords.c
+++ src/setup/passwords.c
@@ -56,7 +56,7 @@ int handleadminpassword(void)
 		return 0;
 	
 	snprintf(commandstring, STRING_SIZE,
-		"/usr/sbin/htpasswd -c -m -b " CONFIG_ROOT "/auth/users admin '%s'", password);
+		"/usr/bin/htpasswd -c -B -C 7 -b " CONFIG_ROOT "/auth/users admin '%s'", password);
 	sprintf(message, _("Setting %s 'admin' user password..."), NAME);
 	if (runhiddencommandwithstatus(commandstring, _("Setting password"), message, NULL)) {
 		sprintf(message, _("Problem setting %s 'admin' user password."), NAME);
