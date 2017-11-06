bb3272dad36da7cbefbbd4a9b0b3d98b760fbd49
Michael Tremer <michael.tremer@ipfire.org>
2017-11-06 18:10:02 +0000

    Start Core Update 117
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- make.sh
+++ make.sh
@@ -25,7 +25,7 @@
 NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 VERSION="2.19"							# Version number
-CORE="116"							# Core Level (Filename)
+CORE="117"							# Core Level (Filename)
 PAKFIRE_CORE="116"						# Core Level (PAKFIRE)
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`			# Git Branch
 SLOGAN="www.ipfire.org"						# Software slogan
