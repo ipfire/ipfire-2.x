592896d2f42bf01fd95d62f8682f51b46edc5efa
Arne Fitzenreiter <arne_f@ipfire.org>
2018-04-05 09:15:32 +0200

    u-boot: update bootscript to boot from other devices than mmc
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- config/u-boot/boot.cmd
+++ config/u-boot/boot.cmd
@@ -1,5 +1,20 @@
+if test ${boot_dev} = ""; then
+	setenv boot_dev mmc;
+	setenv root_dev /dev/mmcblk0p3;
+fi;
+
+if test ${boot_part} = ""; then
+	setenv boot_dev 0:1;
+fi;
+
+if test ${soc} = "kirkwood"; then
+	setenv kernel_type kirkwood;
+else
+	setenv kernel_type multi;
+fi;
+
 # Import uEnv txt...
-if fatload mmc 0 ${kernel_addr_r} uEnv.txt; then
+if fatload ${boot_dev} ${boot_part} ${kernel_addr_r} uEnv.txt; then
 	echo Load uEnv.txt...;
 	env import -t ${kernel_addr_r} ${filesize};
 	if test "${uenvcmd}" = ""; then
@@ -29,22 +44,23 @@ if test "${SERIAL-CONSOLE}" = "ON"; then
 		fi;
 	fi
 	echo Set console to ${console};
-	setenv bootargs console=${console} rootwait root=/dev/mmcblk0p3 rootwait;
+	setenv bootargs console=${console} rootwait root=${root_dev} rootwait;
 else
 	echo Set console to tty1 ;
-	setenv bootargs console=tty1 rootwait root=/dev/mmcblk0p3 rootwait;
+	setenv bootargs console=tty1 rootwait root=${root_dev} rootwait;
 fi;
 
 setenv fdt_high ffffffff;
-fatload mmc 0:1 ${kernel_addr_r} vmlinuz-${KVER}-ipfire-multi;
-fatload mmc 0:1 ${fdt_addr_r} dtb-${KVER}-ipfire-multi/${fdtfile};
-if fatload mmc 0:1 ${ramdisk_addr_r} uInit-${KVER}-ipfire-multi; then
+fatload ${boot_dev} ${boot_part} ${kernel_addr_r} vmlinuz-${KVER}-ipfire-${kernel_type};
+fatload ${boot_dev} ${boot_part} ${fdt_addr_r} dtb-${KVER}-ipfire-${kernel_type}/${fdtfile};
+setenv ramdisk_addr ${ramdisk_addr_r}
+if fatload ${boot_dev} ${boot_part} ${ramdisk_addr} uInit-${KVER}-ipfire-${kernel_type}; then
 	echo Ramdisk loaded...;
 else
 	echo Ramdisk not loaded...;
-	setenv ramdisk_addr_r -;
+	setenv ramdisk_addr -;
 fi ;
-bootz ${kernel_addr_r} ${ramdisk_addr_r} ${fdt_addr_r};
+bootz ${kernel_addr_r} ${ramdisk_addr} ${fdt_addr_r};
 
 # Recompile with:
 # mkimage -C none -A arm -T script -d /boot/boot.cmd /boot/boot.scr 
