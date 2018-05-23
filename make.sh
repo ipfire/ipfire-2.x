6b4174133a4cad58db3570d1cd1a16b243d4da84
Michael Tremer <michael.tremer@ipfire.org>
2018-05-23 12:09:07 +0100

    make.sh: There are no tar.gz images to copy out of the chroot environment
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- make.sh
+++ make.sh
@@ -1582,7 +1582,7 @@ buildpackages() {
 	lfsmake2 flash-images
   fi
 
-  mv $LFS/install/images/{*.iso,*.tgz,*.img.xz,*.bz2} $BASEDIR >> $LOGFILE 2>&1
+  mv $LFS/install/images/{*.iso,*.img.xz,*.bz2} $BASEDIR >> $LOGFILE 2>&1
 
   ipfirepackages
 
