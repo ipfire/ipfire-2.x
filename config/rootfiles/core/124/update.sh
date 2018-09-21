0f2186725ef5cd62d1482b4482e20a6295ebf2ad
Arne Fitzenreiter <arne_f@ipfire.org>
2018-09-21 15:26:52 +0200

    core124: finish updater
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- config/rootfiles/core/124/update.sh
+++ config/rootfiles/core/124/update.sh
@@ -17,7 +17,7 @@
 # along with IPFire; if not, write to the Free Software                    #
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA #
 #                                                                          #
-# Copyright (C) 2017 IPFire-Team <info@ipfire.org>.                        #
+# Copyright (C) 2018 IPFire-Team <info@ipfire.org>.                        #
 #                                                                          #
 ############################################################################
 #
