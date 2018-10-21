c38f55fe23ef870666da0c7dc863002804203338
Michael Tremer <michael.tremer@ipfire.org>
2018-10-21 22:01:28 +0100

    Build openssl earlier in the build process
    This is a basic library that is used by so many other packages
    and more and more are being added. To have those packages take
    advantage of it, we build it very early.
    
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- make.sh
+++ make.sh
@@ -1083,6 +1083,9 @@ buildipfire() {
   lfsmake2 configroot
   lfsmake2 initscripts
   lfsmake2 backup
+  lfsmake2 openssl
+  [ "${BUILD_ARCH}" = "i586" ] && lfsmake2 openssl KCFG='-sse2'
+  lfsmake2 openssl-compat
   lfsmake2 popt
   lfsmake2 libusb
   lfsmake2 libusb-compat
@@ -1169,9 +1172,6 @@ buildipfire() {
   esac
   lfsmake2 intel-microcode
   lfsmake2 xtables-addons			USPACE="1"
-  lfsmake2 openssl
-  [ "${BUILD_ARCH}" = "i586" ] && lfsmake2 openssl KCFG='-sse2'
-  lfsmake2 openssl-compat
   lfsmake2 libgpg-error
   lfsmake2 libgcrypt
   lfsmake2 libassuan
