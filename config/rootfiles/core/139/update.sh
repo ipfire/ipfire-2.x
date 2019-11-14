b1dc936cc60e651773095ceaadbfcbeed2666f84
Arne Fitzenreiter <arne_f@ipfire.org>
2019-11-14 17:28:38 +0000

    rename core138 -> core139 to insert a emergency core update
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- config/rootfiles/core/139/update.sh
+++ config/rootfiles/core/139/update.sh
@@ -24,7 +24,7 @@
 . /opt/pakfire/lib/functions.sh
 /usr/local/bin/backupctrl exclude >/dev/null 2>&1
 
-core=137
+core=139
 
 exit_with_error() {
 	# Set last succesfull installed core.
@@ -41,42 +41,6 @@ for (( i=1; i<=$core; i++ )); do
 	rm -f /var/cache/pakfire/core-upgrade-*-$i.ipfire
 done
 
-KVER="xxxKVERxxx"
-
-# Backup uEnv.txt if exist
-if [ -e /boot/uEnv.txt ]; then
-	cp -vf /boot/uEnv.txt /boot/uEnv.txt.org
-fi
-
-# Do some sanity checks.
-case $(uname -r) in
-	*-ipfire*)
-		# Ok.
-		;;
-	*)
-		exit_with_error "ERROR cannot update. No IPFire Kernel." 1
-		;;
-esac
-
-# Check diskspace on root
-ROOTSPACE=`df / -Pk | sed "s| * | |g" | cut -d" " -f4 | tail -n 1`
-
-if [ $ROOTSPACE -lt 80000 ]; then
-	exit_with_error "ERROR cannot update because not enough free space on root." 2
-	exit 2
-fi
-
-# Remove the old kernel
-rm -rf /boot/System.map-*
-rm -rf /boot/config-*
-rm -rf /boot/ipfirerd-*
-rm -rf /boot/initramfs-*
-rm -rf /boot/vmlinuz-*
-rm -rf /boot/uImage-*-ipfire-*
-rm -rf /boot/zImage-*-ipfire-*
-rm -rf /boot/uInit-*-ipfire-*
-rm -rf /boot/dtb-*-ipfire-*
-rm -rf /lib/modules
 
 # Remove files
 
@@ -92,57 +56,9 @@ ldconfig
 /usr/local/bin/update-lang-cache
 
 # Start services
-/usr/local/bin/ipsecctrl S
-/etc/init.d/suricata restart
-/etc/init.d/unbound restart
-/etc/init.d/collectd restart
 
-# remove lm_sensor config after collectd was started
-# to reserch sensors at next boot with updated kernel
-rm -f  /etc/sysconfig/lm_sensors
-
-# generate new qos script
-/usr/local/bin/qosctrl generate
-
-# Search sensors again after reboot into the new kernel
-rm -f  /etc/sysconfig/lm_sensors
-
-# Upadate Kernel version uEnv.txt
-if [ -e /boot/uEnv.txt ]; then
-	sed -i -e "s/KVER=.*/KVER=${KVER}/g" /boot/uEnv.txt
-fi
-
-# call user update script (needed for some arm boards)
-if [ -e /boot/pakfire-kernel-update ]; then
-	/boot/pakfire-kernel-update ${KVER}
-fi
-
-case "$(uname -m)" in
-	i?86)
-		# Force (re)install pae kernel if pae is supported
-		rm -rf /opt/pakfire/db/installed/meta-linux-pae
-		rm -rf /opt/pakfire/db/rootfiles/linux-pae
-		if [ ! "$(grep "^flags.* pae " /proc/cpuinfo)" == "" ]; then
-			ROOTSPACE=`df / -Pk | sed "s| * | |g" | cut -d" " -f4 | tail -n 1`
-			BOOTSPACE=`df /boot -Pk | sed "s| * | |g" | cut -d" " -f4 | tail -n 1`
-			if [ $BOOTSPACE -lt 22000 -o $ROOTSPACE -lt 120000 ]; then
-				/usr/bin/logger -p syslog.emerg -t ipfire \
-				"core-update-${core}: WARNING not enough space for pae kernel."
-				touch /var/run/need_reboot
-			else
-				echo "Name: linux-pae" > /opt/pakfire/db/installed/meta-linux-pae
-				echo "ProgVersion: 0" >> /opt/pakfire/db/installed/meta-linux-pae
-				echo "Release: 0"     >> /opt/pakfire/db/installed/meta-linux-pae
-			fi
-		else
-			touch /var/run/need_reboot
-		fi
-		;;
-	*)
-		# This update needs a reboot...
-		touch /var/run/need_reboot
-		;;
-esac
+# This update needs a reboot...
+#touch /var/run/need_reboot
 
 # Finish
 /etc/init.d/fireinfo start
