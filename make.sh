c845152b6d4882394f74a5347a785f6a570b6787
Arne Fitzenreiter <arne_f@ipfire.org>
2020-01-26 13:10:13 +0100

    set version and pakfire version to core141
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- make.sh
+++ make.sh
@@ -26,8 +26,8 @@ NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 # If you update the version don't forget to update backupiso and add it to core update
 VERSION="2.25"							# Version number
-CORE="140"							# Core Level (Filename)
-PAKFIRE_CORE="140"						# Core Level (PAKFIRE)
+CORE="141"							# Core Level (Filename)
+PAKFIRE_CORE="141"						# Core Level (PAKFIRE)
 GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`			# Git Branch
 SLOGAN="www.ipfire.org"						# Software slogan
 CONFIG_ROOT=/var/ipfire						# Configuration rootdir
