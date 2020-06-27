5f34a67205dd3205838917c72e2f125eaa19cbd9
Arne Fitzenreiter <arne_f@ipfire.org>
2020-06-27 12:27:10 +0200

    installer: update filecount
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- src/installer/main.c
+++ src/installer/main.c
@@ -23,7 +23,7 @@
 #include <libintl.h>
 #define _(x) dgettext("installer", x)
 
-#define INST_FILECOUNT 24800
+#define INST_FILECOUNT 28000
 #define LICENSE_FILE	"/cdrom/COPYING"
 #define SOURCE_TEMPFILE "/tmp/downloads/image.iso"
 
