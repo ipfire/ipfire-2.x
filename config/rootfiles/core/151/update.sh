a9f69cbf01cd306dbc20c5761eedcdd97bc492e8
Michael Tremer <michael.tremer@ipfire.org>
2020-10-10 11:48:26 +0000

    core151: Apply local SSH configuration
    Fixes: #12494
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- config/rootfiles/core/151/update.sh
+++ config/rootfiles/core/151/update.sh
@@ -50,6 +50,9 @@ ldconfig
 # Filesytem cleanup
 /usr/local/bin/filesystem-cleanup
 
+# Apply local configuration to sshd_config
+/usr/local/bin/sshctrl
+
 # Start services
 /etc/init.d/sshd restart
 if grep -q "ENABLED=on" /var/ipfire/vpn/settings; then
