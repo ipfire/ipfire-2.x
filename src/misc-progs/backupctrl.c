c33f477f5bf1942f70dfaeff793e3d553afe4960
Michael Tremer <michael.tremer@ipfire.org>
2021-01-06 11:17:53 +0000

    misc-progs: backupctrl: Use new run() function
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- src/misc-progs/backupctrl.c
+++ src/misc-progs/backupctrl.c
@@ -5,40 +5,11 @@
  *
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include "setuid.h"
 
-int main(int argc, char *argv[]) {
-	int i;
-	char command[STRING_SIZE] = "/var/ipfire/backup/bin/backup.pl";
-	char temp[STRING_SIZE];
-
-	if (!(initsetuid()))
+int main(int argc, char** argv) {
+	if (!initsetuid())
 		exit(1);
 
-	for (i = 1; i < argc; i++) {
-		if (strstr(argv[i], "&&")){
-			fprintf (stderr, "Bad Argument!\n");
-			exit (1);
-
-		} else if (strstr(argv[i], "|")) {
-			fprintf (stderr, "Bad Argument!\n");
-			exit (1);
-
-		} else if (argc > 3) {
-			fprintf (stderr, "Too Many Arguments!\n");
-			exit (1);
-
-		} else {
-			snprintf(temp, STRING_SIZE, "%s %s", command, argv[i]);
-			snprintf(command, STRING_SIZE, "%s", temp);
-		}
-	}
-
-	return safe_system(command);
+	return run("/var/ipfire/backup/bin/backup.pl", argv);
 }
