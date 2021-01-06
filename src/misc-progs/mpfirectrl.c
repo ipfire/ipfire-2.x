5efedb5487fa6fcc771c03bb9f15b5cb2412416c
Michael Tremer <michael.tremer@ipfire.org>
2021-01-06 11:42:06 +0000

    misc-progs: mpfirectrl: Use new run() function
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- src/misc-progs/mpfirectrl.c
+++ src/misc-progs/mpfirectrl.c
@@ -5,35 +5,11 @@
  *
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include "setuid.h"
 
-int main(int argc, char *argv[]) {
-	int i;
-	char command[1024];
-	char add[STRING_SIZE];
-	
-	if (!(initsetuid()))
+int main(int argc, char** argv) {
+	if (!initsetuid())
 		exit(1);
 
-	snprintf(command, STRING_SIZE, "/var/ipfire/mpfire/bin/mpfire.pl");
-
-	for (i = 1; i < argc; i++) {
-    if (strstr(argv[i], "&&")){
-	    	fprintf (stderr, "Bad Argument!\n");
-        exit (1);
-    }
-		else if (strstr(argv[i], "|")){
-		    fprintf (stderr, "Bad Argument!\n");
-		    exit (1);
-		}
-		sprintf(add, " %s", argv[i]);
-		strcat(command, add);
-	}
-	return safe_system(command);
+	return run("/var/ipfire/mpfire/bin/mpfire.pl", argv);
 }
