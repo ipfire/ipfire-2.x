c651dd9b6b2e00fb10d1ca1a99cf9efaec42df19
Michael Tremer <michael.tremer@ipfire.org>
2021-01-06 11:39:46 +0000

    misc-progs: extrahdctrl: Use new run() function
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- src/misc-progs/extrahdctrl.c
+++ src/misc-progs/extrahdctrl.c
@@ -5,21 +5,11 @@
  *
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <fcntl.h>
 #include "setuid.h"
 
-int main(int argc, char *argv[]) {
-
-	char command[512];
-	if (!(initsetuid()))
+int main(int argc, char** argv) {
+	if (!initsetuid())
 		exit(1);
 
-	snprintf(command, 512, "/var/ipfire/extrahd/bin/extrahd.pl %s %s", argv[1], argv[2]);
-	safe_system("chmod 755 /var/ipfire/extrahd/bin/extrahd.pl 2>&1 >/dev/null");
-	safe_system(command);
+	return run("/var/ipfire/extrahd/bin/extrahd.pl", argv);
 }
