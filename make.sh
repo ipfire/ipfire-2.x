15de7a78feecdfe2fed7ab27d3d8182d8ffa27b3
Peter Müller <peter.mueller@ipfire.org>
2022-01-29 09:40:36 +0000

    Revert "perl-libwww: Update to version 6.60 and rename from libwww-perl to perl-libwww"
    This reverts commit 976b84ccb6303a172416e904b7dc431c7b8c46c4.
    
    See: https://bugzilla.ipfire.org/show_bug.cgi?id=12769#c4
    
    Reported-by: Arne Fitzenreiter <arne.fitzenreiter@ipfire.org>
    Cc: Adolf Belka <adolf.belka@ipfire.org>
    Signed-off-by: Peter Müller <peter.mueller@ipfire.org>

--- make.sh
+++ make.sh
@@ -1301,7 +1301,7 @@ buildipfire() {
   lfsmake2 Digest
   lfsmake2 Digest-SHA1
   lfsmake2 Digest-HMAC
-  lfsmake2 perl-libwww
+  lfsmake2 libwww-perl
   lfsmake2 Net-DNS
   lfsmake2 Net-IPv4Addr
   lfsmake2 Net_SSLeay
