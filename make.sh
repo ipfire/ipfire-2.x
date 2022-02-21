91f907df8e42429fca91d0470ad3bbdfbf1cb925
Michael Tremer <michael.tremer@ipfire.org>
2022-02-21 18:43:15 +0000

    python3-arrow: New package
    Required by oci-cli.
    
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- make.sh
+++ make.sh
@@ -1640,6 +1640,7 @@ buildipfire() {
   lfsmake2 python3-circuitbreaker
   lfsmake2 python3-pytz
   lfsmake2 python3-click
+  lfsmake2 python3-arrow
   lfsmake2 aws-cli
   lfsmake2 oci-python-sdk
   lfsmake2 oci-cli
