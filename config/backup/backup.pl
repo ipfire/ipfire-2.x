ipfire/ipfire-2.x

commit c7e0d73e7cfd7be95db9d0a5f3392b8241813d5b

backup: Make include/exclude files relative
Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

config/backup/backup.pl
@@ -39,7 +39,7 @@ process_includes() {
 		local file
 		while read -r file; do
 			for file in ${file}; do
-				if [ -e "${file}" ]; then
+				if [ -e "/${file}" ]; then
 					echo "${file}"
 				fi
 			done
@@ -58,7 +58,7 @@ make_backup() {
 	done
 
 	# Backup using global exclude/include definitions
-	tar cvfz "${filename}" \
+	tar cvfz "${filename}" -C / \
 		--exclude-from="/var/ipfire/backup/exclude" \
 		--exclude-from="/var/ipfire/backup/exclude.user" \
 		$(process_includes "/var/ipfire/backup/include") \

config/backup/exclude
@@ -1,10 +1,10 @@
-/etc/sysconfig/lm_sensors
-/etc/unbound/unbound.conf
+etc/sysconfig/lm_sensors
+etc/unbound/unbound.conf
 *.tmp
-/var/ipfire/ethernet/settings
-/var/ipfire/firewall/bin/*
-/var/ipfire/ovpn/openssl/*
-/var/ipfire/proxy/calamaris/bin/*
-/var/ipfire/qos/bin/qos.pl
-/var/ipfire/suricata/oinkmaster.conf
-/var/ipfire/urlfilter/blacklists/*/*.db
+var/ipfire/ethernet/settings
+var/ipfire/firewall/bin/*
+var/ipfire/ovpn/openssl/*
+var/ipfire/proxy/calamaris/bin/*
+var/ipfire/qos/bin/qos.pl
+var/ipfire/suricata/oinkmaster.conf
+var/ipfire/urlfilter/blacklists/*/*.db

config/backup/include
@@ -1,63 +1,63 @@
-/etc/conntrackd/conntrackd.conf
-/etc/group
-/etc/hosts*
-/etc/httpd/server.crt
-/etc/httpd/server.csr
-/etc/httpd/server-ecdsa.crt
-/etc/httpd/server-ecdsa.csr
-/etc/httpd/server-ecdsa.key
-/etc/httpd/server.key
-/etc/ipsec.user.*
-/etc/ipsec.user-post.conf
-/etc/logrotate.d
-/etc/passwd
-/etc/shadow
-/etc/ssh/sshd_config
-/etc/ssh/ssh_host*
-/etc/squid/squid.conf.local
-/etc/squid/squid.conf.pre.local
-/etc/sysconfig/*
-/etc/sysconfig/firewall.local
-/etc/sysconfig/rc.local
-/etc/unbound
-/root/.bash_history
-/root/.gitconfig
-/root/.ssh
-/var/ipfire/auth/users
-/var/ipfire/backup/addons/backup
-/var/ipfire/backup/exclude.user
-/var/ipfire/backup/include.user
-/var/ipfire/captive/*
-/var/ipfire/*/*.conf
-/var/ipfire/*/config
-/var/ipfire/dhcp/*
-/var/ipfire/dns
-/var/ipfire/dnsforward/*
-/var/ipfire/*/enable
-/var/ipfire/*/*enable*
-/var/ipfire/ethernet/aliases
-/var/ipfire/ethernet/wireless
-/var/ipfire/firewall
-/var/ipfire/fwhosts
-/var/ipfire/main/*
-/var/ipfire/ovpn
-/var/ipfire/ovpn/collectd.vpn
-/var/ipfire/*/*.pem
-/var/ipfire/ppp
-/var/ipfire/proxy
-/var/ipfire/qos/*
-/var/ipfire/qos/bin/qos.sh
-/var/ipfire/suricata/*.conf
-/var/ipfire/suricata/*.yaml
-/var/ipfire/suricata/providers-settings
-/var/ipfire/*/settings
-/var/ipfire/time/
-/var/ipfire/urlfilter
-/var/ipfire/vpn
-/var/lib/suricata
-/var/log/ip-acct/*
-/var/log/rrd/*
-/var/log/rrd/collectd
-/var/log/vnstat
-/var/tmp/idsrules-*.tar.gz
-/var/tmp/idsrules-*.rules
+etc/conntrackd/conntrackd.conf
+etc/group
+etc/hosts*
+etc/httpd/server.crt
+etc/httpd/server.csr
+etc/httpd/server-ecdsa.crt
+etc/httpd/server-ecdsa.csr
+etc/httpd/server-ecdsa.key
+etc/httpd/server.key
+etc/ipsec.user.*
+etc/ipsec.user-post.conf
+etc/logrotate.d
+etc/passwd
+etc/shadow
+etc/ssh/sshd_config
+etc/ssh/ssh_host*
+etc/squid/squid.conf.local
+etc/squid/squid.conf.pre.local
+etc/sysconfig/*
+etc/sysconfig/firewall.local
+etc/sysconfig/rc.local
+etc/unbound
+root/.bash_history
+root/.gitconfig
+root/.ssh
+var/ipfire/auth/users
+var/ipfire/backup/addons/backup
+var/ipfire/backup/exclude.user
+var/ipfire/backup/include.user
+var/ipfire/captive/*
+var/ipfire/*/*.conf
+var/ipfire/*/config
+var/ipfire/dhcp/*
+var/ipfire/dns
+var/ipfire/dnsforward/*
+var/ipfire/*/enable
+var/ipfire/*/*enable*
+var/ipfire/ethernet/aliases
+var/ipfire/ethernet/wireless
+var/ipfire/firewall
+var/ipfire/fwhosts
+var/ipfire/main/*
+var/ipfire/ovpn
+var/ipfire/ovpn/collectd.vpn
+var/ipfire/*/*.pem
+var/ipfire/ppp
+var/ipfire/proxy
+var/ipfire/qos/*
+var/ipfire/qos/bin/qos.sh
+var/ipfire/suricata/*.conf
+var/ipfire/suricata/*.yaml
+var/ipfire/suricata/providers-settings
+var/ipfire/*/settings
+var/ipfire/time/
+var/ipfire/urlfilter
+var/ipfire/vpn
+var/lib/suricata
+var/log/ip-acct/*
+var/log/rrd/*
+var/log/rrd/collectd
+var/log/vnstat
+var/tmp/idsrules-*.tar.gz
+var/tmp/idsrules-*.rules

commit 13f6473a4f2fc2678a8d1199fc6dcb542c95fff8

backup: Fix broken globbing expansion
This patch fixes globbing expansion in the backup include file list
which got broken in c7e0d73e7cfd7be95db9d0a5f3392b8241813d5b.

Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>
Tested-by: Adolf Belka <adolf.belka@ipfire.org>
Tested-by: Bernhard Bitsch <bernhard.bitsch@ipfire.org>

config/backup/backup.pl
@@ -19,6 +19,8 @@
 #                                                                             #
 ###############################################################################
 
+shopt -s nullglob
+
 NOW="$(date "+%Y-%m-%d-%H:%M")"
 
 list_addons() {
@@ -38,10 +40,8 @@ process_includes() {
 	for include in $@; do
 		local file
 		while read -r file; do
-			for file in ${file}; do
-				if [ -e "/${file}" ]; then
-					echo "${file}"
-				fi
+			for file in /${file}; do
+				echo "${file}"
 			done
 		done < "${include}"
 	done | sort -u