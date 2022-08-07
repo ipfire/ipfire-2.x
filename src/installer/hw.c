06cb5c1bb7cdfafb8bf0bcb0b6d00c5548958806
Michael Tremer <michael.tremer@ipfire.org>
2022-08-07 12:00:55 +0000

    installer: Increase size of /boot to 256 MiB
    On some installations, we are running out of space on the /boot
    partition due to growing sizes of the ramdisk and the kernel.
    
    To accomodate for that and have room to grow in the future, we increase
    the size of the partition to 256 MiB.
    
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>
    Reviewed-by: Peter Müller <peter.mueller@ipfire.org>

--- src/installer/hw.c
+++ src/installer/hw.c
@@ -492,7 +492,7 @@ static unsigned long long hw_swap_size(struct hw_destination* dest) {
 }
 
 static unsigned long long hw_boot_size(struct hw_destination* dest) {
-	return MB2BYTES(128);
+	return MB2BYTES(256);
 }
 
 static int hw_device_has_p_suffix(const struct hw_destination* dest) {
