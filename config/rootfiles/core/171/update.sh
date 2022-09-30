2f0fb4492901771f6a830876155f3caf0b9d560b
Peter Müller <peter.mueller@ipfire.org>
2022-09-30 15:48:03 +0000

    Core Update 171: Ship and restart Suricata
    Signed-off-by: Peter Müller <peter.mueller@ipfire.org>

--- config/rootfiles/core/171/update.sh
+++ config/rootfiles/core/171/update.sh
@@ -50,6 +50,7 @@ done
 /usr/local/bin/openvpnctrl -k
 /usr/local/bin/openvpnctrl -kn2n
 /etc/rc.d/init.d/ipsec stop
+/etc/init.d/rc.d/suricata stop
 /etc/rc.d/init.d/collectd stop
 
 KVER="xxxKVERxxx"
@@ -226,6 +227,9 @@ chown -v root:root /var/ipfire/backup/{in,ex}clude*
 
 # Start services
 /etc/rc.d/init.d/collectd start
+if grep -q "ENABLED=on" /var/ipfire/suricata/settings; then
+	/etc/rc.d/init.d/suricata start
+fi
 /etc/rc.d/init.d/unbound start
 /etc/rc.d/init.d/apache start
 if grep -q "ENABLED=on" /var/ipfire/ovpn/settings; then
