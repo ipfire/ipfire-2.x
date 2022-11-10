f9ab4c432aa681ce03a80e8c58bce237d6d982ef
Peter Müller <peter.mueller@ipfire.org>
2022-11-10 15:26:00 +0000

    Core Update 172: Ship Python 3.10.8 and related changes
    Signed-off-by: Peter Müller <peter.mueller@ipfire.org>

--- config/rootfiles/core/172/update.sh
+++ config/rootfiles/core/172/update.sh
@@ -43,6 +43,33 @@ rm -rvf \
 	/usr/lib/libexpat.so.1.8.9 \
 	/usr/lib/libunbound.so.8.1.1* \
 	/usr/lib/libxml2.so.2.9.* \
+	/usr/lib/python3.10/ensurepip/_bundled/pip-21* \
+	/usr/lib/python3.10/ensurepip/_bundled/setuptools-5* \
+	/usr/lib/python3.10/lib2to3/Grammar3.10.* \
+	/usr/lib/python3.10/lib2to3/PatternGrammar3.10.* \
+	/usr/lib/python3.10/site-packages/pip-21.* \
+	/usr/lib/python3.10/site-packages/pip/_internal/utils/parallel.py \
+	/usr/lib/python3.10/site-packages/pip/_internal/utils/pkg_resources.py \
+	/usr/lib/python3.10/site-packages/pip/_vendor/appdirs.py \
+	/usr/lib/python3.10/site-packages/pip/_vendor/chardet/compat.py \
+	/usr/lib/python3.10/site-packages/pip/_vendor/distlib/_backport \
+	/usr/lib/python3.10/site-packages/pip/_vendor/distro.py \
+	/usr/lib/python3.10/site-packages/pip/_vendor/html5lib \
+	/usr/lib/python3.10/site-packages/pip/_vendor/msgpack/_version.py \
+	/usr/lib/python3.10/site-packages/pip/_vendor/progress \
+	/usr/lib/python3.10/site-packages/pip/_vendor/pyparsing.py \
+	/usr/lib/python3.10/site-packages/pip/_vendor/urllib3/packages/ssl_match_hostname \
+	/usr/lib/python3.10/site-packages/pkg_resources/_vendor/packaging/_compat.py \
+	/usr/lib/python3.10/site-packages/pkg_resources/_vendor/packaging/_typing.py \
+	/usr/lib/python3.10/site-packages/pkg_resources/_vendor/pyparsing.py \
+	/usr/lib/python3.10/site-packages/pkg_resources/tests/data \
+	/usr/lib/python3.10/site-packages/setuptools-5* \
+	/usr/lib/python3.10/site-packages/setuptools/_distutils/py35compat.py \
+	/usr/lib/python3.10/site-packages/setuptools/_vendor/packaging/_compat.py \
+	/usr/lib/python3.10/site-packages/setuptools/_vendor/packaging/_typing.py \
+	/usr/lib/python3.10/site-packages/setuptools/_vendor/pyparsing.py \
+	/usr/lib/python3.10/site-packages/setuptools/config.py \
+	/usr/lib/python3.10/site-packages/setuptools_rust/utils.py \
 	/usr/libexec/ipsec/scepclient
 
 # Remove powertop add-on, if installed
