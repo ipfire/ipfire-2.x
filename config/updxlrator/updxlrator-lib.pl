362c5537afd468e479275dc4ced9363c50d25be2
Peter Müller <peter.mueller@ipfire.org>
2022-11-18 13:33:45 +0000

    Ensure /var/ipfire/updatexlrator/updxlrator-lib.pl is not writable by "nobody"
    Signed-off-by: Peter Müller <peter.mueller@ipfire.org>

--- config/updxlrator/updxlrator-lib.pl
+++ config/updxlrator/updxlrator-lib.pl
@@ -1,16 +1,23 @@
 #!/usr/bin/perl
-#
-# This code is distributed under the terms of the GPL
-#
-# (c) 2006-2008 marco.s - http://update-accelerator.advproxy.net
-#
-# Portions (c) 2008 by dotzball - http://www.blockouttraffic.de
-#
-# dotzball 2008-05-27:
-#		move functions from all local files to one library file
-#
-# $Id: updxlrator-lib.pl,v 1.1 2008/11/29 00:00:00 marco.s Exp $
-#
+###############################################################################
+#                                                                             #
+# IPFire.org - A linux based firewall                                         #
+# Copyright (C) 2007-2022  IPFire Team  <info@ipfire.org>                     #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#                                                                             #
+###############################################################################
 
 package UPDXLT;
 
