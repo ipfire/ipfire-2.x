8f591d729f209fc1d243a18850a9cd321212f2a4
Peter Müller <peter.mueller@ipfire.org>
2022-11-28 13:17:07 +0100

    Core Update 172: Ship libtiff
    Signed-off-by: Peter Müller <peter.mueller@ipfire.org>

--- config/rootfiles/core/172/update.sh
+++ config/rootfiles/core/172/update.sh
@@ -87,6 +87,8 @@ rm -rvf \
 	/usr/lib/libpng16.so.16.37.0 \
 	/usr/lib/libreadline.so.8.1 \
 	/usr/lib/libtasn1.so.6.6.2 \
+	/usr/lib/libtiff.so.5.7* \
+	/usr/lib/libtiffxx.so.5.7* \
 	/usr/lib/libunbound.so.8.1.1* \
 	/usr/lib/libxml2.so.2.9.* \
 	/usr/lib/python3.10/ensurepip/_bundled/pip-21* \
