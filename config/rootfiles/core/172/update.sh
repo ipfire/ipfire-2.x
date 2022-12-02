dc9f7554c13ebdadb45f27c8f38f14b9d16468d2
Peter Müller <peter.mueller@ipfire.org>
2022-12-02 17:43:04 +0100

    Core Update 172: Zut alors, keep libhistory.so.8.1 around as well
    My fault, again. :-/
    
    Reported-by: Adolf Belka <adolf.belka@ipfire.org>
    Signed-off-by: Peter Müller <peter.mueller@ipfire.org>

--- config/rootfiles/core/172/update.sh
+++ config/rootfiles/core/172/update.sh
@@ -77,7 +77,6 @@ rm -rvf \
 	/usr/lib/libdns-9.16.33.so \
 	/usr/lib/libedit.so.0.0.6* \
 	/usr/lib/libexpat.so.1.8.9 \
-	/usr/lib/libhistory.so.8.1 \
 	/usr/lib/libirs-9.16.33.so \
 	/usr/lib/libisc-9.16.33.so \
 	/usr/lib/libisccc-9.16.33.so \
