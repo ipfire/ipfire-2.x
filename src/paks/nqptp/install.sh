758951bf25d48e8906e2cc7e378ac02be26f6224
Michael Tremer <michael.tremer@ipfire.org>
2023-01-19 13:22:19 +0000

    nqptp: Update to latest version in main branch
    This patch also creates symlinks that the daemon is automatically
    started.
    
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>
    Acked-by: Peter Müller <peter.mueller@ipfire.org>

---
+++ src/paks/nqptp/install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+############################################################################
+#                                                                          #
+# This file is part of the IPFire Firewall.                                #
+#                                                                          #
+# IPFire is free software; you can redistribute it and/or modify           #
+# it under the terms of the GNU General Public License as published by     #
+# the Free Software Foundation; either version 2 of the License, or        #
+# (at your option) any later version.                                      #
+#                                                                          #
+# IPFire is distributed in the hope that it will be useful,                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+# GNU General Public License for more details.                             #
+#                                                                          #
+# You should have received a copy of the GNU General Public License        #
+# along with IPFire; if not, write to the Free Software                    #
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA #
+#                                                                          #
+# Copyright (C) 2007 IPFire-Team <info@ipfire.org>.                        #
+#                                                                          #
+############################################################################
+#
+. /opt/pakfire/lib/functions.sh
+
+# Extract everything
+extract_files
+
+# Enable auto-start for service
+ln -sf  ../init.d/nqptp /etc/rc.d/rc0.d/K21nqptp
+ln -sf  ../init.d/nqptp /etc/rc.d/rc3.d/S98nqptp
+ln -sf  ../init.d/nqptp /etc/rc.d/rc6.d/K21nqptp
+
+exit 0
