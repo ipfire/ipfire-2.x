0179bff8eaab08d175dc903b338cfeec7e2b1615
Peter Müller <peter.mueller@ipfire.org>
2023-04-19 10:21:34 +0000

    Core Update 175: Ship kernel and regenerate initial ramdisks locally
    Signed-off-by: Peter Müller <peter.mueller@ipfire.org>

--- config/rootfiles/core/175/update.sh
+++ config/rootfiles/core/175/update.sh
@@ -26,6 +26,18 @@
 
 core=175
 
+exit_with_error() {
+    # Set last succesfull installed core.
+    echo $(($core-1)) > /opt/pakfire/db/core/mine
+    # force fsck at next boot, this may fix free space on xfs
+    touch /forcefsck
+    # don't start pakfire again at error
+    killall -KILL pak_update
+    /usr/bin/logger -p syslog.emerg -t ipfire \
+	"core-update-${core}: $1"
+    exit $2
+}
+
 # Remove old core updates from pakfire cache to save space...
 for (( i=1; i<=$core; i++ )); do
 	rm -f /var/cache/pakfire/core-upgrade-*-$i.ipfire
@@ -34,6 +46,44 @@ done
 # Stop services
 /etc/rc.d/init.d/apache stop
 /etc/rc.d/init.d/sshd stop
+
+KVER="xxxKVERxxx"
+
+# Backup uEnv.txt if exist
+if [ -e /boot/uEnv.txt ]; then
+    cp -vf /boot/uEnv.txt /boot/uEnv.txt.org
+fi
+
+# Do some sanity checks prior to the kernel update
+case $(uname -r) in
+    *-ipfire*)
+	# Ok.
+	;;
+    *)
+	exit_with_error "ERROR cannot update. No IPFire Kernel." 1
+	;;
+esac
+
+# Check diskspace on root
+ROOTSPACE=$( df / -Pk | sed "s| * | |g" | cut -d" " -f4 | tail -n 1 )
+
+if [ $ROOTSPACE -lt 100000 ]; then
+    exit_with_error "ERROR cannot update because not enough free space on root." 2
+    exit 2
+fi
+
+# Remove the old kernel
+rm -rvf \
+	/boot/System.map-* \
+	/boot/config-* \
+	/boot/ipfirerd-* \
+	/boot/initramfs-* \
+	/boot/vmlinuz-* \
+	/boot/uImage-* \
+	/boot/zImage-* \
+	/boot/uInit-* \
+	/boot/dtb-* \
+	/lib/modules
 
 # Extract files
 extract_files
@@ -69,8 +119,31 @@ if grep -q "ENABLE_SSH=on" /var/ipfire/remote/settings; then
 	/etc/init.d/sshd start
 fi
 
+# Regenerate all initrds
+dracut --regenerate-all --force
+case "$(uname -m)" in
+	aarch64)
+		mkimage -A arm64 -T ramdisk -C lzma -d /boot/initramfs-${KVER}-ipfire.img /boot/uInit-${KVER}-ipfire
+		# dont remove initramfs because grub need this to boot.
+		;;
+esac
+
+# remove lm_sensor config after collectd was started
+# to re-search sensors at next boot with updated kernel
+rm -f  /etc/sysconfig/lm_sensors
+
+# Upadate Kernel version in uEnv.txt
+if [ -e /boot/uEnv.txt ]; then
+    sed -i -e "s/KVER=.*/KVER=${KVER}/g" /boot/uEnv.txt
+fi
+
+# Call user update script (needed for some ARM boards)
+if [ -e /boot/pakfire-kernel-update ]; then
+    /boot/pakfire-kernel-update ${KVER}
+fi
+
 # This update needs a reboot...
-#touch /var/run/need_reboot
+touch /var/run/need_reboot
 
 # Finish
 /etc/init.d/fireinfo start
