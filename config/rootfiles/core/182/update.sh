71070690da3fa83c1f948c2c577cb9e171078da9
Arne Fitzenreiter <arne_f@ipfire.org>
2023-12-06 13:27:11 +0000

    core182: dont remove libs in update
    this will delete the current liblzma library.
    
    Unused libs removed by filesystem cleanup later.
    
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- config/rootfiles/core/182/update.sh
+++ config/rootfiles/core/182/update.sh
@@ -63,10 +63,7 @@ rm -rvf \
 	/lib/firmware/yamaha \
 	/lib/udev/rules.d/64-xfs.rules \
 	/usr/bin/gawk-5.2* \
-	/usr/lib/grub/{arm64-efi,i386-pc,riscv64-efi,x86_64-efi}/gmodule.pl \
-	/usr/lib/liblzma.so.5.4* \
-	/usr/lib/libqpdf.so.29.5* \
-	/usr/lib/libsodium.so.23*
+	/usr/lib/grub/{arm64-efi,i386-pc,riscv64-efi,x86_64-efi}/gmodule.pl
 
 # update linker config
 ldconfig
