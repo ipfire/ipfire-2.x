ed5e80eb626db0c6dd96f5a4c4dbe3fb7cd05fc2
Arne Fitzenreiter <arne_f@ipfire.org>
2024-01-10 07:22:59 +0100

    grub: update to 2.12 (final release version)
    this should fix problems on systems installed on xfs
    
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- config/rootfiles/core/183/update.sh
+++ config/rootfiles/core/183/update.sh
@@ -17,7 +17,7 @@
 # along with IPFire; if not, write to the Free Software                    #
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA #
 #                                                                          #
-# Copyright (C) 2023 IPFire-Team <info@ipfire.org>.                        #
+# Copyright (C) 2024 IPFire-Team <info@ipfire.org>.                        #
 #                                                                          #
 ############################################################################
 #
@@ -149,7 +149,7 @@ sendprofile
 
 # Update grub config to display new core version
 if [ -e /boot/grub/grub.cfg ]; then
-	grub-mkconfig -o /boot/grub/grub.cfg
+<------>/usr/bin/install-bootloader
 fi
 
 sync
