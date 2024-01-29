ccaa26aa6a169ac7430dd2ea025075231b74d012
Adolf Belka <adolf.belka@ipfire.org>
2024-01-29 14:41:12 +0100

    python3-pathspec: New build dependency for python3-hatchling
    - lfs and rootfile created.
    - rootfile put into common as it is only used as a build dependency.
    - Used pyproject.toml build approach
    
    Tested-by: Adolf Belka <adolf.belka@ipfire.org>
    Signed-off-by: Adolf Belka <adolf.belka@ipfire.org>
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- make.sh
+++ make.sh
@@ -1596,6 +1596,7 @@ buildipfire() {
   lfsmake2 python3-pkgconfig
   lfsmake2 python3-msgpack
   lfsmake2 python3-editables
+  lfsmake2 python3-pathspec
   lfsmake2 python3-hatchling
   lfsmake2 python3-hatch-vcs
   lfsmake2 python3-hatch-fancy-pypi-readme
