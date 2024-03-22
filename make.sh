790c04a6eaafcf695315dcd3f4f39ec6b35be48c
Arne Fitzenreiter <arne_f@ipfire.org>
2024-03-22 06:39:34 +0100

    core186: start update
    Signed-off-by: Arne Fitzenreiter <arne_f@ipfire.org>

--- make.sh
+++ make.sh
@@ -23,7 +23,7 @@ NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 # If you update the version don't forget to update backupiso and add it to core update
 VERSION="2.29"							# Version number
-CORE="185"							# Core Level (Filename)
+CORE="186"							# Core Level (Filename)
 SLOGAN="www.ipfire.org"						# Software slogan
 CONFIG_ROOT=/var/ipfire						# Configuration rootdir
 MAX_RETRIES=1							# prefetch/check loop
