d289bc28beb65a937aba335501a86102d0455cec
Jonatan Schlag <jonatan.schlag@ipfire.org>
2024-06-16 18:02:39 +0200

    initscripts fkt: Ignore comments in readhash
    As '#Another Comment' is a valid key we test this change by checking if
    the comments do not end up as keys in our array.
    
    Signed-off-by: Jonatan Schlag <jonatan.schlag@ipfire.org>
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- tests/src/initscripts/system/functions/test.sh
+++ tests/src/initscripts/system/functions/test.sh
@@ -15,5 +15,11 @@ readhash "CONFIG" "${SCRIPT_PATH}/data/1"
 test_value_in_array "CONFIG" "RED_DHCP_HOSTNAME" "ipfire"
 test_value_in_array "CONFIG" "BLUE_MACADDR" "bc:30:7d:58:6b:e3"
 
+# Test that comments are skipped
+# apparently the way we read the file strips the whitespace, so the key does not contain any whitespace either
+test_that_array_doesnt_have_key "CONFIG" "# Another Comment"
+test_that_array_doesnt_have_key "CONFIG" "# Comment for testing"
+test_that_array_doesnt_have_key "CONFIG" "# Comment for testing Comments with spaces before"
+
 test_that_output_is "${SCRIPT_PATH}/data/1_output_stdout" "1" readhash "CONFIG" "${SCRIPT_PATH}/data/1"
 test_that_output_is "${SCRIPT_PATH}/data/1_output_stderr" "2" readhash "CONFIG" "${SCRIPT_PATH}/data/1"
