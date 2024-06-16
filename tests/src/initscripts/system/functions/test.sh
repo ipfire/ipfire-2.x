02254f55438a47b94b5be763fb9d96e48983000a
Jonatan Schlag <jonatan.schlag@ipfire.org>
2024-06-16 18:02:40 +0200

    initscripts fkt: ignore invalid keys in readhash
    Signed-off-by: Jonatan Schlag <jonatan.schlag@ipfire.org>
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- tests/src/initscripts/system/functions/test.sh
+++ tests/src/initscripts/system/functions/test.sh
@@ -23,3 +23,19 @@ test_that_array_doesnt_have_key "CONFIG" "# Comment for testing Comments with sp
 
 test_that_output_is "${SCRIPT_PATH}/data/1_output_stdout" "1" readhash "CONFIG" "${SCRIPT_PATH}/data/1"
 test_that_output_is "${SCRIPT_PATH}/data/1_output_stderr" "2" readhash "CONFIG" "${SCRIPT_PATH}/data/1"
+
+# Check with invalid Lines (values and keys)
+readhash "CONFIG2" "${SCRIPT_PATH}/data/2" &> /dev/null
+
+# test if we read the correct data
+test_value_in_array "CONFIG2" "RED_DHCP_HOSTNAME" "ipfire"
+test_value_in_array "CONFIG2" "BLUE_MACADDR" "bc:30:7d:58:6b:e3"
+
+# We could do some complex checking if we would create functions to check for correct values and keys.
+# We would be then able to mock these function and check if they are correctly called and if the data
+# does not end up in our array.
+# I think the more simpler way of checking the logged errors is the fastes way here.
+test_that_output_is "${SCRIPT_PATH}/data/2_output_stdout" "1" readhash "CONFIG2" "${SCRIPT_PATH}/data/2"
+test_that_output_is "${SCRIPT_PATH}/data/2_output_stderr" "2" readhash "CONFIG2" "${SCRIPT_PATH}/data/2"
+
+
