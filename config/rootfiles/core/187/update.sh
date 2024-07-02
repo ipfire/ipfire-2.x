981bdaead80d99b01f9e3361acde5760d847e6ff
Michael Tremer <michael.tremer@ipfire.org>
2024-07-02 09:04:14 +0000

    core187: Regenerate initramdisks for intel-microcode
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- config/rootfiles/core/187/update.sh
+++ config/rootfiles/core/187/update.sh
@@ -52,6 +52,16 @@ ldconfig
 # Start services
 /etc/init.d/unbound restart
 
+# Build initial ramdisks (for intel-microcode)
+dracut --regenerate-all --force
+KVER="xxxKVERxxx"
+case "$(uname -m)" in
+	aarch64)
+		mkimage -A arm64 -T ramdisk -C lzma -d /boot/initramfs-${KVER}-ipfire.img /boot/uInit-${KVER}-ipfire
+		# dont remove initramfs because grub need this to boot.
+		;;
+esac
+
 # This update needs a reboot...
 touch /var/run/need_reboot
 
