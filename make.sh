a1f7518cef7451b3de439b25d12842a6d9c48885
Michael Tremer <michael.tremer@ipfire.org>
2024-08-02 17:01:41 +0000

    make.sh: Bump the toolchain version
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- make.sh
+++ make.sh
@@ -33,7 +33,7 @@ GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"			# Git Branch
 GIT_TAG="$(git tag | tail -1)"					# Git Tag
 GIT_LASTCOMMIT="$(git rev-parse --verify HEAD)"			# Last commit
 
-TOOLCHAINVER=20240521
+TOOLCHAINVER=20240802
 
 ###############################################################################
 #
