76da4bd66c8da9a203a21e607c88a0874e9c3a27
Michael Tremer <michael.tremer@ipfire.org>
2025-04-22 14:09:03 +0000

    Start Core Update 195
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- make.sh
+++ make.sh
@@ -23,7 +23,7 @@ NAME="IPFire"							# Software name
 SNAME="ipfire"							# Short name
 # If you update the version don't forget to update backupiso and add it to core update
 VERSION="2.29"							# Version number
-CORE="194"							# Core Level (Filename)
+CORE="195"							# Core Level (Filename)
 SLOGAN="www.ipfire.org"						# Software slogan
 CONFIG_ROOT=/var/ipfire						# Configuration rootdir
 
