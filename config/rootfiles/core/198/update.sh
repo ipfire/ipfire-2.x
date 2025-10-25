05877ded9d658eb051b4482833bc333e57e5ab29
Michael Tremer <michael.tremer@ipfire.org>
2025-10-25 12:55:42 +0000

    core198: Ship changes in apache configuration
    Signed-off-by: Michael Tremer <michael.tremer@ipfire.org>

--- config/rootfiles/core/198/update.sh
+++ config/rootfiles/core/198/update.sh
@@ -75,6 +75,7 @@ telinit u
 # Start services
 /etc/init.d/suricata restart
 /etc/init.d/fcron restart
+/etc/init.d/apache reload
 
 # Build initial ramdisks (for intel-microcode)
 dracut --regenerate-all --force
